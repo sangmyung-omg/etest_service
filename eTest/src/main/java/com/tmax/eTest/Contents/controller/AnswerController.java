e9d65eaa7199c668de28727866e8c5d46c277054
김성진 <sungjin_kim2@tmax.co.kr>
2021-06-01 13:27:01 +0900

    [feat] 해설 받아오기 서비스 추가

--- eTest/src/main/java/com/tmax/eTest/Contents/controller/AnswerController.java
+++ eTest/src/main/java/com/tmax/eTest/Contents/controller/AnswerController.java
@@ -1,5 +1,6 @@
 package com.tmax.eTest.Contents.controller;
 
+import java.util.HashMap;
 import java.util.Map;
 import org.json.simple.JSONObject;
 import org.json.simple.parser.JSONParser;
@@ -9,6 +10,7 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import com.tmax.eTest.Contents.answer.service.AnswerServices;
+import com.tmax.eTest.Contents.exception.problem.NoDataException;
 import com.tmax.eTest.Contents.service.problem.ProblemServices;
 
 
@@ -39,8 +41,21 @@ public class AnswerController {
 		} else {
 			return false;
 		}
-			
+	}
+	
+	@GetMapping(value="/problems/{id}/solution", produces = "application/json; charset=utf-8")
+	public Map<String, Object> problem(@PathVariable("id") long id) throws Exception{
+		Map<String, Object> output = new HashMap<String, Object>();
+		Map<String, Object> data = null;
+		try {
+			data = answerServices.getSolutionMaterial(id);
+			output.put("resultMessage", "success");
+			output.put("data", data);
+		}catch(NoDataException e) {
+			output.put("resultMessage", "Failed: "+e.getMessage());
+		}
 		
+		return output;
 	}
 
 }
