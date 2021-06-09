c8daeeceef7ff13dfbd1f19bf223276f361ddce8
김성진 <sungjin_kim2@tmax.co.kr>
2021-06-09 15:07:50 +0900

    [feat] 브랜치 살리기

--- eTest/src/main/java/com/tmax/eTest/Contents/controller/AnswerController.java
+++ eTest/src/main/java/com/tmax/eTest/Contents/controller/AnswerController.java
@@ -5,15 +5,24 @@ import java.util.Map;
 import org.json.simple.JSONObject;
 import org.json.simple.parser.JSONParser;
 import org.springframework.beans.factory.annotation.Autowired;
-
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpMethod;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.http.client.HttpComponentsClientHttpRequestFactory;
+import org.springframework.util.LinkedMultiValueMap;
+import org.springframework.util.MultiValueMap;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
-
+import org.springframework.web.client.RestTemplate;
+import org.springframework.web.util.UriComponents;
+import org.springframework.web.util.UriComponentsBuilder;
 
 import com.tmax.eTest.Contents.exception.problem.NoDataException;
-
+import com.tmax.eTest.Contents.model.LRS;
 import com.tmax.eTest.Contents.service.AnswerServices;
 import com.tmax.eTest.Contents.service.ProblemServices;
 
@@ -64,64 +73,64 @@ public class AnswerController {
 		
 		return output;
 	}
-//	
-//	@GetMapping(value="/test", produces = "application/json; charset=utf-8")
-//	public boolean test(
-//			@RequestParam String actionType, 
-//			@RequestParam String isCorrect, 
-//			@RequestParam String sourceType, 
-//			@RequestParam String timestamp,  
-//			@RequestParam String userId
-//			) 
-//	{
-//		final String url = "http://192.168.153.132:8080/SaveStatement";
+	
+	@GetMapping(value="/test", produces = "application/json; charset=utf-8")
+	public boolean test(
+			@RequestParam String actionType, 
+			@RequestParam String isCorrect, 
+			@RequestParam String sourceType, 
+			@RequestParam String timestamp,  
+			@RequestParam String userId
+			) 
+	{
+		final String url = "http://192.168.153.132:8080/SaveStatement";
+		
+		RestTemplate restTemplate = new RestTemplate();
+		
+		
+//		LRS lrs= new LRS(actionType,isCorrect,sourceType,timestamp,userId);
+		
+//		Map<String, String> map = new HashMap<>();
 //		
-//		RestTemplate restTemplate = new RestTemplate();
+//		map.put("actionType", actionType);
+//		map.put("isCorrect", isCorrect);
+//		map.put("sourceType", sourceType);
+//		map.put("timestamp", timestamp);
+//		map.put("userId", userId);
 //		
+		MultiValueMap<String, String> params = new LinkedMultiValueMap<String, String>();
+		
+		params.add("actionType", actionType);
+		params.add("isCorrect", isCorrect);
+		params.add("sourceType", sourceType);
+		params.add("timestamp", timestamp);
+		params.add("userId", userId);
+		
+		HttpHeaders headers = new HttpHeaders();
+		headers.setContentType(MediaType.APPLICATION_JSON);
+		
+//		HttpEntity<Map<String,String>> entity = new HttpEntity<Map<String,String>>(map,headers);
+		HttpEntity<MultiValueMap<String, String>> request = new HttpEntity<MultiValueMap<String, String>>(params, headers);
+		
+//		ResponseEntity<String> response = restTemplate.exchange(url,
+//				HttpMethod.POST,
+//				entity,
+//				String.class);
+		
+//		System.out.println(response.getBody());
+		
+		
+		
+		
 //		
-////		LRS lrs= new LRS(actionType,isCorrect,sourceType,timestamp,userId);
-//		
-////		Map<String, String> map = new HashMap<>();
-////		
-////		map.put("actionType", actionType);
-////		map.put("isCorrect", isCorrect);
-////		map.put("sourceType", sourceType);
-////		map.put("timestamp", timestamp);
-////		map.put("userId", userId);
-////		
-//		MultiValueMap<String, String> params = new LinkedMultiValueMap<String, String>();
-//		
-//		params.add("actionType", actionType);
-//		params.add("isCorrect", isCorrect);
-//		params.add("sourceType", sourceType);
-//		params.add("timestamp", timestamp);
-//		params.add("userId", userId);
-//		
-//		HttpHeaders headers = new HttpHeaders();
-//		headers.setContentType(MediaType.APPLICATION_JSON);
-//		
-////		HttpEntity<Map<String,String>> entity = new HttpEntity<Map<String,String>>(map,headers);
-//		HttpEntity<MultiValueMap<String, String>> request = new HttpEntity<MultiValueMap<String, String>>(params, headers);
-//		
-////		ResponseEntity<String> response = restTemplate.exchange(url,
-////				HttpMethod.POST,
-////				entity,
-////				String.class);
-//		
-////		System.out.println(response.getBody());
-//		
-//		
-//		
-////		
-////		ResponseEntity<String> result = restTemplate.exchange(url, HttpMethod.POST,params,String.class);
-//		
-//		ResponseEntity<String> result = restTemplate.postForEntity(url, request, String.class);
-//
-//		System.out.println("Result = " + result.getBody());
-//		
-//		
-//		
-//		return true;
-//	}
+//		ResponseEntity<String> result = restTemplate.exchange(url, HttpMethod.POST,params,String.class);
+		ResponseEntity<String> result = restTemplate.postForEntity(url, request, String.class);
+
+		System.out.println("Result = " + result.getBody());
+		
+		
+		
+		return true;
+	}
 
 }
