03d8529445289a0efe901bae173d79f22120f024
김성진 <sungjin_kim2@tmax.co.kr>
2021-06-17 13:33:04 +0900

    [feat] UK_UUID -> UK_ID 로 변경

--- eTest/src/main/java/com/tmax/eTest/Contents/controller/ProblemController.java
+++ eTest/src/main/java/com/tmax/eTest/Contents/controller/ProblemController.java
@@ -1,20 +1,16 @@
 package com.tmax.eTest.Contents.controller; 
 
 import java.nio.charset.Charset;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
-import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.tmax.eTest.Contents.dto.problem.DiagnosisProblemDTO;
@@ -25,7 +21,7 @@ import com.tmax.eTest.Contents.model.DiagnosisProblemBody;
 import com.tmax.eTest.Contents.model.TestProblemBody;
 import com.tmax.eTest.Contents.service.ProblemServices;
 
-
+@CrossOrigin(origins="*")
 @RestController
 public class ProblemController {
 	
