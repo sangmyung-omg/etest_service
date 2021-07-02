96884f911deb0554add9fdcdd50efe718d937ba4
오성돈 <sungdon_oj@tmax.co.kr>
2021-07-02 15:20:08 +0900

    [feat] 미니테스트 파트별 이해도, UK 부분 적용.

--- eTest/src/main/java/com/tmax/eTest/Report/service/MiniTestReportService.java
+++ eTest/src/main/java/com/tmax/eTest/Report/service/MiniTestReportService.java
@@ -41,250 +41,230 @@ import com.tmax.eTest.Test.repository.UserKnowledgeRepository;
 
 @Service
 public class MiniTestReportService {
-	
+
 	@Autowired
 	LRSAPIManager lrsAPIManager;
 	@Autowired
 	TritonAPIManager tritonAPIManager;
-	
+
 	@Autowired
 	StateAndProbProcess stateAndProbProcess;
 	@Autowired
 	RuleBaseScoreCalculator scoreCalculator;
-	
+
 	@Autowired
 	ProblemRepository problemRepo;
 	@Autowired
 	UserKnowledgeRepository userKnowledgeRepo;
 	@Autowired
 	UserEmbeddingRepository userEmbeddingRepo;
-	
+
 	private final Logger logger = LoggerFactory.getLogger(this.getClass().getSimpleName());
 
-	public MiniTestResultDTO getMiniTestResult(String userId)
-	{
+	public MiniTestResultDTO getMiniTestResult(String userId) {
 		MiniTestResultDTO result = new MiniTestResultDTO();
 		result.initForDummy();
-		
+
 		// Mini Test 관련 문제 풀이 정보 획득.
 		List<StatementDTO> miniTestRes = getMiniTestResultInLRS(userId);
 		List<Problem> probInfos = getProblemInfos(miniTestRes);
 		Map<String, Object> probInfoForTriton = stateAndProbProcess.makeInfoForTriton(miniTestRes, probInfos);
 		TritonResponseDTO tritonResponse = getUnderstandingScoreInTriton(probInfoForTriton);
-		
-		
+
 		TritonDataDTO embeddingData = null;
 		TritonDataDTO masteryData = null;
-		
-		for(TritonDataDTO dto : tritonResponse.getOutputs())
-		{
-			if(dto.getName().equals("Embeddings"))
-			{
+
+		for (TritonDataDTO dto : tritonResponse.getOutputs()) {
+			if (dto.getName().equals("Embeddings")) {
 				embeddingData = dto;
-			}
-			else if(dto.getName().equals("Mastery"))
-			{
+			} else if (dto.getName().equals("Mastery")) {
 				masteryData = dto;
 			}
 		}
-		
-		
+
 		int diagQuestionInfo[] = stateAndProbProcess.calculateDiagQuestionInfo(miniTestRes);
 		result.setDiagnosisQuestionInfo(diagQuestionInfo);
-		
-		if(embeddingData != null && masteryData != null)
-		{
+
+		if (embeddingData != null && masteryData != null) {
+			@SuppressWarnings("unchecked")
+			Map<Integer, UkMaster> ukMap = (Map<Integer, UkMaster>) probInfoForTriton
+					.get(stateAndProbProcess.UK_MAP_KEY);
+
 			Map<Integer, Float> ukScore = makeUKScore(masteryData);
-			List<List<String>> partScoreList = makePartScore(ukScore);
-			List<List<String>> weakPartDetail = makeWeakPartDetail(ukScore, partScoreList);
-			
+			List<List<String>> partScoreList = makePartScore(ukMap, ukScore);
+			List<List<String>> weakPartDetail = makeWeakPartDetail(ukMap, ukScore, partScoreList);
+
 			result.setPartUnderstanding(partScoreList);
 			result.setWeakPartDetail(weakPartDetail);
-			
+
 			float avg = 0;
-			for(List<String> part : partScoreList)
-			{
+			for (List<String> part : partScoreList) {
 				avg += Float.parseFloat(part.get(2));
 			}
-			
-			result.setScore(Math.round(avg/partScoreList.size()));
-			
+
+			result.setScore(Math.round(avg / partScoreList.size()));
+
 			saveUKAndEmbedding(userId, ukScore, embeddingData);
 		}
-		
+
 		return result;
 	}
-	
-	
-	
-	
+
 	// partScore example ["partname", "C", "50"]
-	private List<List<String>> makeWeakPartDetail(Map<Integer, Float> ukScore, List<List<String>> partScore)
-	{
+	private List<List<String>> makeWeakPartDetail(
+			Map<Integer, UkMaster> ukMap, 
+			Map<Integer, Float> ukScore, 
+			List<List<String>> partScore) {
+		
 		List<List<String>> result = new ArrayList<>();
 		Pair<String, Integer> lowestScorePart = Pair.of("NULL", 100);
-		
+
 		// check lowest score
-		for(List<String> part : partScore)
-		{
+		for (List<String> part : partScore) {
 			String partName = part.get(0);
 			int score = Integer.parseInt(part.get(2));
-			
-			if(lowestScorePart.getSecond() > score)
+
+			if (lowestScorePart.getSecond() > score)
 				lowestScorePart = Pair.of(partName, score);
 		}
-		
+
 		// 파트 구불법 필요.
 		String lowScorePartName = lowestScorePart.getFirst();
-		int lowScore = lowestScorePart.getSecond();
-		ukScore.forEach((uuid, score) ->{
-			
-			if( (lowScorePartName.equals("Part 1") && (uuid == 20 || uuid == 13 || uuid == 148))
-				|| ( lowScorePartName.equals("Part 2") && (uuid == 217 || uuid == 254 || uuid == 234 || uuid == 235) ))
-			{
+		ukScore.forEach((ukId, score) -> {
+			UkMaster recentUK = ukMap.get(ukId);
+			if (recentUK != null 
+				&& lowScorePartName.equals(recentUK.getPart())) {
 				// example
 				List<String> partUK = new ArrayList<>();
-				
-				partUK.add("UK "+uuid);
-				partUK.add(scoreCalculator.calculateUKScoreString(lowScore));
+
+				partUK.add(recentUK.getUkName());
+				//partUK.add(ukScore.get(ukId).toString());
+				partUK.add(scoreCalculator.calculateUKScoreString(ukScore.get(ukId) * 100));
 				partUK.add("C");
-				
+
 				result.add(partUK);
 			}
 
 		});
-		
-		
+
 		return result;
 	}
-	
-	private List<List<String>> makePartScore(Map<Integer, Float> ukScore)
-	{
+
+	private List<List<String>> makePartScore(Map<Integer, UkMaster> ukMap, Map<Integer, Float> ukScore) {
 		List<List<String>> result = new ArrayList<>();
 		Map<String, Pair<Float, Integer>> partInfo = new HashMap<>();
-		
-		
+
 		// 파트 구분법 필요.
 		ukScore.forEach((ukUuid, score) -> {
-			String partName = "Part 1";
-			if(ukUuid > 150)
-				partName = "Part 2";
-			
-			if(partInfo.get(partName) != null)
-			{
-				Pair<Float, Integer> scoreInfo = partInfo.get(partName);
-				
-				int partNum = scoreInfo.getSecond()+1;
-				float avg = (scoreInfo.getFirst()*scoreInfo.getSecond()+score*100)/(float)partNum;
-				
-				partInfo.put(partName, Pair.of(avg, partNum));
-				
+			UkMaster ukInfo = ukMap.get(ukUuid);
+			if (ukInfo != null) {
+				String partName = ukInfo.getPart();
+
+				if (partInfo.get(partName) != null) {
+					Pair<Float, Integer> scoreInfo = partInfo.get(partName);
+
+					int partNum = scoreInfo.getSecond() + 1;
+					float avg = (scoreInfo.getFirst() * scoreInfo.getSecond() + score * 100) / (float) partNum;
+
+					partInfo.put(partName, Pair.of(avg, partNum));
+
+				} else {
+					Pair<Float, Integer> scoreInfo = Pair.of(score * 100, 1);
+					partInfo.put(partName, scoreInfo);
+				}
 			}
-			else
-			{
-				Pair<Float, Integer> scoreInfo = Pair.of(score*100, 1);
-				partInfo.put(partName, scoreInfo);
-			}
-	
+
 		});
-		
-		partInfo.forEach((part, info) ->{
+
+		partInfo.forEach((part, info) -> {
 			List<String> partScore = new ArrayList<>();
 			partScore.add(part);
-			
-			if(info.getFirst() >= 85.f)
+
+			if (info.getFirst() >= 85.f)
 				partScore.add("A");
-			else if(info.getFirst() >= 70.f)
+			else if (info.getFirst() >= 70.f)
 				partScore.add("B");
-			else if(info.getFirst() >= 50.f)
+			else if (info.getFirst() >= 50.f)
 				partScore.add("C");
-			else if(info.getFirst() >= 30.f)
+			else if (info.getFirst() >= 30.f)
 				partScore.add("D");
-			else if(info.getFirst() >= 15.f)
+			else if (info.getFirst() >= 15.f)
 				partScore.add("E");
 			else
 				partScore.add("F");
-			
+
 			partScore.add(String.valueOf(Math.round(info.getFirst())));
-			
+
 			result.add(partScore);
 		});
-		
+
 		return result;
 	}
-	
-	private Map<Integer, Float> makeUKScore(TritonDataDTO mastery)
-	{
+
+	private Map<Integer, Float> makeUKScore(TritonDataDTO mastery) {
 		Map<Integer, Float> result = new HashMap<Integer, Float>();
-		
+
 		JsonObject masteryJson = (JsonObject) JsonParser.parseString((String) mastery.getData().get(0));
 
 		masteryJson.keySet().forEach(ukId -> {
 			int ukUuid = Integer.parseInt(ukId);
-			result.put(ukUuid, masteryJson.get(ukId).getAsFloat());	
+			result.put(ukUuid, masteryJson.get(ukId).getAsFloat());
 		});
-		
+
 		return result;
 	}
-	
-	private List<StatementDTO> getMiniTestResultInLRS(String userID)
-	{
+
+	private List<StatementDTO> getMiniTestResultInLRS(String userID) {
 		List<StatementDTO> result = new ArrayList<>();
 		GetStatementInfoDTO statementInput = new GetStatementInfoDTO();
 		statementInput.pushUserId(userID);
 		statementInput.pushSourceType("mini_test_question");
 		statementInput.pushActionType("submit");
-		
+
 		try {
 			result = lrsAPIManager.getStatementList(statementInput);
 		} catch (ParseException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
-		
+
 		return result;
 	}
-	
-	private List<Problem> getProblemInfos(List<StatementDTO> miniTestResult)
-	{
+
+	private List<Problem> getProblemInfos(List<StatementDTO> miniTestResult) {
 		List<Integer> probIdList = new ArrayList<>();
 		List<Problem> probList = new ArrayList<>();
-		
-		for(StatementDTO dto : miniTestResult)
-		{
-			try
-			{
+
+		for (StatementDTO dto : miniTestResult) {
+			try {
 				int probId = Integer.parseInt(dto.getSourceId());
 				probIdList.add(probId);
 				probList = problemRepo.findAllById(probIdList);
-			}
-			catch(Exception e)
-			{
-				logger.info("getUnderstandingScoreInTriton : "+e.toString()+" id : "+dto.getSourceId()+" error!");
+			} catch (Exception e) {
+				logger.info(
+						"getUnderstandingScoreInTriton : " + e.toString() + " id : " + dto.getSourceId() + " error!");
 			}
 		}
-		
+
 		// problem 관련 정보를 가공하여 TritonInput 화.
 		return probList;
 	}
-	
+
 	@SuppressWarnings("unchecked")
-	private TritonResponseDTO getUnderstandingScoreInTriton(
-			Map<String, Object> probInfoForTriton)
-	{
+	private TritonResponseDTO getUnderstandingScoreInTriton(Map<String, Object> probInfoForTriton) {
 		// first process : 문제별 PK 얻어오기.
-		
+
 		TritonRequestDTO tritonReq = new TritonRequestDTO();
-		
+
 		tritonReq.initDefault();
-		
-		tritonReq.pushInputData("UKList", "INT32", 
-				(List<Object>)probInfoForTriton.get(stateAndProbProcess.UK_LIST_KEY));
-		tritonReq.pushInputData("IsCorrectList", "INT32", 
-				(List<Object>)probInfoForTriton.get(stateAndProbProcess.IS_CORRECT_LIST_KEY));
-		tritonReq.pushInputData("DifficultyList", "INT32", 
-				(List<Object>)probInfoForTriton.get(stateAndProbProcess.DIFF_LIST_KEY));
-		
+
+		tritonReq.pushInputData("UKList", "INT32",
+				(List<Object>) probInfoForTriton.get(stateAndProbProcess.UK_LIST_KEY));
+		tritonReq.pushInputData("IsCorrectList", "INT32",
+				(List<Object>) probInfoForTriton.get(stateAndProbProcess.IS_CORRECT_LIST_KEY));
+		tritonReq.pushInputData("DifficultyList", "INT32",
+				(List<Object>) probInfoForTriton.get(stateAndProbProcess.DIFF_LIST_KEY));
+
 		// Triton에 데이터 요청.
 		TritonResponseDTO tritonResponse = null;
 		try {
@@ -293,21 +273,20 @@ public class MiniTestReportService {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
-		
+
 		return tritonResponse;
 	}
-	
-	private void saveUKAndEmbedding(String userId, Map<Integer,Float> ukScoreList, TritonDataDTO embedding)
-	{
+
+	private void saveUKAndEmbedding(String userId, Map<Integer, Float> ukScoreList, TritonDataDTO embedding) {
 //		String userEmbedding = (String) embedding.getData().get(0);
 //		UserEmbedding updateEmbedding = new UserEmbedding();
 //		updateEmbedding.setUserUuid(userId);
 //		updateEmbedding.setUserEmbedding(userEmbedding);
 //		updateEmbedding.setUpdateDate(Timestamp.valueOf(LocalDateTime.now()));
 //		logger.info(updateEmbedding.toString());
-		
+
 		Set<UserKnowledge> userKnowledgeSet = new HashSet<UserKnowledge>();
-		
+
 		ukScoreList.forEach((ukUuid, score) -> {
 			UserKnowledge userKnowledge = new UserKnowledge();
 			userKnowledge.setUserUuid(userId);
@@ -315,17 +294,15 @@ public class MiniTestReportService {
 			userKnowledge.setUkMastery(score);
 			userKnowledge.setUpdateDate(Timestamp.valueOf(LocalDateTime.now()));
 			userKnowledgeSet.add(userKnowledge);
-	
+
 		});
-		
+
 		try {
-			//userEmbeddingRepo.save(updateEmbedding);
+			// userEmbeddingRepo.save(updateEmbedding);
 			userKnowledgeRepo.saveAll(userKnowledgeSet);
-		}
-		catch(Exception e)
-		{
+		} catch (Exception e) {
 			logger.info(e.toString());
-		}	
+		}
 	}
-	
+
 }
