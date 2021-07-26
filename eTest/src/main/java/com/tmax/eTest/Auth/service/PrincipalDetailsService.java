dd46d284af4abf68c8c164e277f96be9af8efc98
minjoon_lee <minjoon1995@naver.com>
2021-07-26 18:53:35 +0900

    [fix] @RequiredArgsConstructor로 IOC하면 Qualifier이 적용안된다 따라서 @Autowired로 적용

--- eTest/src/main/java/com/tmax/eTest/Auth/service/PrincipalDetailsService.java
+++ eTest/src/main/java/com/tmax/eTest/Auth/service/PrincipalDetailsService.java
@@ -1,9 +1,10 @@
 package com.tmax.eTest.Auth.service;
 
 import com.tmax.eTest.Auth.dto.PrincipalDetails;
-import com.tmax.eTest.Test.model.UserMaster;
-import com.tmax.eTest.Test.repository.UserRepository;
+import com.tmax.eTest.Auth.repository.UserRepository;
+import com.tmax.eTest.Common.model.user.UserMaster;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
@@ -12,6 +13,7 @@ import org.springframework.stereotype.Service;
 @Service
 public class PrincipalDetailsService implements UserDetailsService {
     @Autowired
+    @Qualifier("AU-UserRepository")
     private UserRepository userRepository;
 
     @Override
