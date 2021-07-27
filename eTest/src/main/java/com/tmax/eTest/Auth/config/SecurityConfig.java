d19c66847565f3df4ad0c3ddee1d251c960f30d5
minjoon_lee <minjoon1995@naver.com>
2021-07-27 12:42:52 +0900

    [feature] configure에서 cors해제

--- eTest/src/main/java/com/tmax/eTest/Auth/config/SecurityConfig.java
+++ eTest/src/main/java/com/tmax/eTest/Auth/config/SecurityConfig.java
@@ -2,11 +2,13 @@ package com.tmax.eTest.Auth.config;
 
 import com.tmax.eTest.Auth.service.OAuth2DetailsService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
 @RequiredArgsConstructor
@@ -16,6 +18,8 @@ import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
     private final OAuth2DetailsService oAuth2DetailsService;
 
+    @Autowired
+    private CorsConfig corsConfig;
 
     @Bean
     public BCryptPasswordEncoder encode() {
@@ -24,16 +28,16 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
-        http.csrf().disable();
+        http
+                .addFilter(corsConfig.corsFilter())
+                .csrf().disable()
+                .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS);
         http.authorizeRequests()
                 .antMatchers("/", "/user/**", "/image/**", "/subscribe/**", "/comment/**","/api/**").authenticated()
                 .anyRequest().permitAll()
                 .and()
-                .formLogin()
-                .loginPage("/auth/signin")
-                .loginProcessingUrl("/auth/signin") //POST로 오면 낚아챈다.
-                .defaultSuccessUrl("/")
-                .and()
+                .formLogin().disable()
+                .httpBasic().disable()
                 .oauth2Login()//form 로그인도하고, oauth2도 한다.
                 .userInfoEndpoint()//최종응답으로 회원정보를 바로 받는다.
                 .userService(oAuth2DetailsService);
