793617b87b9c24f2b42731e3c16f76eab550321a
minjoon_lee <minjoon1995@naver.com>
2021-07-27 12:44:13 +0900

    [fix] CorsConfig class 파일 rename

--- eTest/src/main/java/com/tmax/eTest/Auth/config/CorsConfig.java
+++ eTest/src/main/java/com/tmax/eTest/Auth/config/CorsConfig.java
@@ -6,7 +6,7 @@ import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 import org.springframework.web.filter.CorsFilter;
 @Configuration
-public class Corsconfig {
+public class CorsConfig {
     @Bean
     public CorsFilter corsFilter() {
         UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
