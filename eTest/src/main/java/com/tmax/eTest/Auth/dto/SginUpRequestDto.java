017ce46b61af604aec326510a7d6d3297a228b1c
minjoon_lee <minjoon1995@naver.com>
2021-07-28 20:23:05 +0900

    [feature] RequestDto와 Exception추가

---
+++ eTest/src/main/java/com/tmax/eTest/Auth/dto/SginUpRequestDto.java
@@ -0,0 +1,4 @@
+package com.tmax.eTest.Auth.dto;
+
+public class SginUpRequestDto {
+}
---
+++ eTest/src/main/java/com/tmax/eTest/Auth/dto/SignUpRequestDto.java
@@ -0,0 +1,18 @@
+package com.tmax.eTest.Auth.dto;
+
+import lombok.Data;
+
+import javax.validation.constraints.Email;
+import javax.validation.constraints.NotBlank;
+@Data
+public class SignUpRequestDto {
+    @NotBlank
+    private String name;
+
+    @NotBlank
+    @Email
+    private String email;
+
+    @NotBlank
+    private String password;
+}
