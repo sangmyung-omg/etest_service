509b9ae257b0488769266cfc38bdbd59f718210c
minjoon_lee <minjoon1995@naver.com>
2021-07-28 20:23:36 +0900

    [feature] 필요한 RequestDto와 Exceoption 더 추가

---
+++ eTest/src/main/java/com/tmax/eTest/Auth/dto/ApiResponseDto.java
@@ -0,0 +1,13 @@
+package com.tmax.eTest.Auth.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class ApiResponseDto {
+    private boolean success;
+    private String message;
+}
