77b1bb4b4c0cf1410d869ead6fc837c608af3197
minjoon_lee <minjoon1995@naver.com>
2021-07-28 20:29:17 +0900

    [fix] Security에 구글과 페이스북 ClientRegistration추가 및 Role기능 임시로 삭제

--- eTest/src/main/java/com/tmax/eTest/Auth/config/SecurityConfig.java
+++ eTest/src/main/java/com/tmax/eTest/Auth/config/SecurityConfig.java
@@ -1,45 +1,164 @@
 package com.tmax.eTest.Auth.config;
 
+import com.tmax.eTest.Auth.jwt.JwtCommonAuthorizationFilter;
+import com.tmax.eTest.Auth.jwt.JwtTokenProvider;
+import com.tmax.eTest.Auth.repository.UserRepository;
 import com.tmax.eTest.Auth.service.OAuth2DetailsService;
+import com.tmax.eTest.Auth.service.PrincipalDetailsService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.autoconfigure.security.oauth2.client.OAuth2ClientProperties;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.BeanIds;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.config.oauth2.client.CommonOAuth2Provider;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.AuthenticationException;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.oauth2.client.registration.ClientRegistration;
+import org.springframework.security.oauth2.client.registration.ClientRegistrationRepository;
+import org.springframework.security.oauth2.client.registration.InMemoryClientRegistrationRepository;
+import org.springframework.security.web.authentication.AuthenticationFailureHandler;
+import org.springframework.security.web.authentication.AuthenticationSuccessHandler;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
-@RequiredArgsConstructor
-@EnableWebSecurity //해당 파일로 security를 활성화 시킨다
-@Configuration //IOC
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.util.List;
+import java.util.Objects;
+import java.util.stream.Collectors;
 
-public class SecurityConfig extends WebSecurityConfigurerAdapter {
-    private final OAuth2DetailsService oAuth2DetailsService;
+@Configuration
+@EnableWebSecurity
+@EnableGlobalMethodSecurity(
+        securedEnabled = true,
+        jsr250Enabled = true,
+        prePostEnabled = true)
+
+public class SecurityConfig extends WebSecurityConfigurerAdapter{
 
     @Autowired
-    private CorsConfig corsConfig;
+    private PrincipalDetailsService principalDetailsService;
+
+    @Autowired
+    private OAuth2DetailsService oAuth2DetailsService;
+
+    @Autowired
+    private UserRepository userRepository;
+
+    @Autowired
+    private JwtTokenProvider tokenProvider;
 
     @Bean
-    public BCryptPasswordEncoder encode() {
+    public BCryptPasswordEncoder encodePWD() {
         return new BCryptPasswordEncoder();
     }
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
-        http
-                .addFilter(corsConfig.corsFilter())
-                .csrf().disable()
-                .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS);
+        http.csrf().disable(); //csrf 토큰
+        http.cors();
+        http.sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS);
+        http.formLogin().disable();
+        http.httpBasic().disable();
+//        http.addFilter(new JwtBasicAuthenticationFilter());
+        http.addFilter(new JwtCommonAuthorizationFilter(authenticationManager(), tokenProvider, userRepository));
+
         http.authorizeRequests()
-                .antMatchers("/", "/user/**", "/image/**", "/subscribe/**", "/comment/**","/api/**").authenticated()
+                .antMatchers("/user/**")
+                .authenticated()
+                .antMatchers("/admin/**").access("hasRole('ROLE_ADMIN')")
                 .anyRequest().permitAll()
                 .and()
-                .formLogin().disable()
-                .httpBasic().disable()
-                .oauth2Login()//form 로그인도하고, oauth2도 한다.
-                .userInfoEndpoint()//최종응답으로 회원정보를 바로 받는다.
-                .userService(oAuth2DetailsService);
+                .oauth2Login()
+                .userInfoEndpoint()
+                .userService(oAuth2DetailsService)
+                .and()
+                .successHandler(new AuthenticationSuccessHandler() {
+
+                    @Override
+                    public void onAuthenticationSuccess(HttpServletRequest request, HttpServletResponse response,
+                                                        Authentication authentication) throws IOException, ServletException {
+                        String token = tokenProvider.create(authentication);
+                        System.out.println("=================================");
+                        System.out.println(token);
+                        System.out.println("=================================");
+                        response.addHeader("Authorization", "Bearer " +  token);
+                        String targetUrl = "/auth/success";
+                        RequestDispatcher dis = request.getRequestDispatcher(targetUrl);
+                        dis.forward(request, response);
+                    }
+                })
+                .failureHandler(new AuthenticationFailureHandler() {
+                    @Override
+                    public void onAuthenticationFailure(HttpServletRequest request, HttpServletResponse response,
+                                                        AuthenticationException exception) throws IOException, ServletException {
+                        response.sendError(HttpServletResponse.SC_UNAUTHORIZED, "Unauthorized");
+                    }
+                });
     }
+
+
+    @Override
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+        auth.userDetailsService(principalDetailsService).passwordEncoder(encodePWD());
+    }
+
+    @Bean
+    @Override
+    public AuthenticationManager authenticationManagerBean() throws Exception {
+        return super.authenticationManagerBean();
+    }
+
+    @Bean
+    public ClientRegistrationRepository clientRegistrationRepository(OAuth2ClientProperties clientProperties){
+
+        List<ClientRegistration> registrations =
+                clientProperties.getRegistration().keySet().stream()
+                        .map(provider -> getRegistration(clientProperties, provider))
+                        .filter(Objects::nonNull)
+                        .collect(Collectors.toList());
+
+        return new InMemoryClientRegistrationRepository(registrations);
+    }
+
+
+    private ClientRegistration getRegistration(OAuth2ClientProperties clientProperties, String provider) {
+        if("google".equals(provider)) {
+            OAuth2ClientProperties.Registration registration = clientProperties.getRegistration()
+                    .get("google");
+
+            return CommonOAuth2Provider.GOOGLE.getBuilder(provider)
+                    .clientId(registration.getClientId())
+                    .clientSecret(registration.getClientSecret())
+                    .scope("email", "profile")
+                    .build();
+        }
+
+        if("facebook".equals(provider)) {
+            OAuth2ClientProperties.Registration registration = clientProperties.getRegistration()
+                    .get("facebook");
+
+            return CommonOAuth2Provider.FACEBOOK.getBuilder(provider)
+                    .clientId(registration.getClientId())
+                    .clientSecret(registration.getClientSecret())
+                    .userInfoUri("https://graph.facebook.com/me?fields=id,name,email,link")
+                    .scope("email")
+                    .build();
+        }
+        return null;
+
+    }
+
 }
