a2aa3208de0939930d218513c1b276d2477e4e34
minjoon_lee <minjoon1995@naver.com>
2021-08-02 21:49:20 +0900

    [fix] Gender를 Enum으로 바꿈

---
+++ eTest/src/main/java/com/tmax/eTest/Auth/dto/Gender.java
@@ -0,0 +1,13 @@
+package com.tmax.eTest.Auth.dto;
+
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+
+@Getter
+@RequiredArgsConstructor
+public enum Gender {
+    male,
+    female;
+}
+
+
