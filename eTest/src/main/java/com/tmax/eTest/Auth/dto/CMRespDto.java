32b466acf16d3e3c6f4a64419019f85e15172b40
minjoon_lee <minjoon1995@naver.com>
2021-08-02 21:50:02 +0900

    [feature] 결과를 response하는 CMresponseDto 추가

---
+++ eTest/src/main/java/com/tmax/eTest/Auth/dto/CMRespDto.java
@@ -0,0 +1,14 @@
+package com.tmax.eTest.Auth.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class CMRespDto<T> {
+    private int code; // 200 성공 500 실패
+    private String message;
+    private T data;
+}
