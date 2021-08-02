7562e9b8e2bf0e0938f1cad96c90f78e5e35b569
minjoon_lee <minjoon1995@naver.com>
2021-08-02 21:54:29 +0900

    [fix] config에서 Oauth2기능 지움

--- eTest/src/main/java/com/tmax/eTest/Auth/config/SecurityConfig.java
+++ eTest/src/main/java/com/tmax/eTest/Auth/config/SecurityConfig.java
@@ -1,11 +1,8 @@
 package com.tmax.eTest.Auth.config;
 
-import com.tmax.eTest.Auth.jwt.JwtBasicAuthenticationFilter;
 import com.tmax.eTest.Auth.jwt.JwtCommonAuthorizationFilter;
 import com.tmax.eTest.Auth.jwt.JwtTokenProvider;
-//import com.tmax.eTest.Auth.model.CustomOAuth2Provider;
 import com.tmax.eTest.Auth.repository.UserRepository;
-import com.tmax.eTest.Auth.service.OAuth2DetailsService;
 import com.tmax.eTest.Auth.service.PrincipalDetailsService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -55,9 +52,6 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter{
     private PrincipalDetailsService principalDetailsService;
 
     @Autowired
-    private OAuth2DetailsService oAuth2DetailsService;
-
-    @Autowired
     private UserRepository userRepository;
 
     @Autowired
@@ -75,8 +69,7 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter{
         http.sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS);
         http.formLogin().disable();
         http.httpBasic().disable();
-//        http.addFilter(new JwtBasicAuthenticationFilter());
-        http.addFilter(new JwtCommonAuthorizationFilter(authenticationManager(), tokenProvider, userRepository));
+        http.addFilter(new JwtCommonAuthorizationFilter(authenticationManager(),  userRepository));
 
         http.authorizeRequests()
 
@@ -84,107 +77,4 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter{
                 .antMatchers("/admin/**").access("hasRole('ROLE_ADMIN')")
                 .anyRequest().permitAll();
     }
-//                .and()
-//                .oauth2Login()
-//                .userInfoEndpoint()
-//                .userService(oAuth2DetailsService)
-//                .and()
-//                .successHandler(new AuthenticationSuccessHandler() {
-//
-//                    @Override
-//                    public void onAuthenticationSuccess(HttpServletRequest request, HttpServletResponse response,
-//                                                        Authentication authentication) throws IOException, ServletException {
-//                        String token = tokenProvider.create(authentication);
-//                        response.addHeader("Authorization", "Bearer " +  token);
-//                        String targetUrl = "/auth/success";
-//                        RequestDispatcher dis = request.getRequestDispatcher(targetUrl);
-//                        dis.forward(request, response);
-//                    }
-//                })
-//                .failureHandler(new AuthenticationFailureHandler() {
-//                    @Override
-//                    public void onAuthenticationFailure(HttpServletRequest request, HttpServletResponse response,
-//                                                        AuthenticationException exception) throws IOException, ServletException {
-//                        response.sendError(HttpServletResponse.SC_UNAUTHORIZED, "Unauthorized");
-//                    }
-//                });
-//    }
-//
-//
-//    @Override
-//    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
-//        auth.userDetailsService(principalDetailsService).passwordEncoder(encodePWD());
-//    }
-//
-//    @Bean
-//    @Override
-//    public AuthenticationManager authenticationManagerBean() throws Exception {
-//        return super.authenticationManagerBean();
-//    }
-
-
-
-
-// 위는 이번에 추가
-//        **************************************************************************
-// 아래는 원래 주석
-
-
-
-
-
-//
-//    @Bean
-//    public ClientRegistrationRepository clientRegistrationRepository(OAuth2ClientProperties clientProperties,
-//             @Value("${custom.oauth2.kakao.client-id}") String kakaoClientId,
-//             @Value("${custom.oauth2.kakao.client-secret}") String kakaoClientSecret,
-//             @Value("${custom.oauth2.naver.client-id}") String naverClientId,
-//             @Value("${custom.oauth2.naver.client-secret}") String naverClientSecret)
-//
-//{
-//        System.out.println(naverClientId);
-//        List<ClientRegistration> registrations = clientProperties.getRegistration().keySet().stream()
-//                        .map(provider -> getRegistration(clientProperties, provider))
-//                        .filter(Objects::nonNull)
-//                        .collect(Collectors.toList());
-//    registrations.add(CustomOAuth2Provider.KAKAO.getBuilder("kakao")
-//            .clientId(kakaoClientId) .clientSecret(kakaoClientSecret)
-//            .jwkSetUri("http://localhost:8080") .build());
-//    registrations.add(CustomOAuth2Provider.NAVER.getBuilder("naver")
-//            .clientId(naverClientId) .clientSecret(naverClientSecret)
-//            .jwkSetUri("http://localhost:8080") .build());
-//
-//    return new InMemoryClientRegistrationRepository(registrations);
-//    }
-//
-//
-//    private ClientRegistration getRegistration(OAuth2ClientProperties clientProperties, String provider) {
-//        if("google".equals(provider)) {
-//            OAuth2ClientProperties.Registration registration = clientProperties.getRegistration()
-//                    .get("google");
-//
-//            return CommonOAuth2Provider.GOOGLE.getBuilder(provider)
-//                    .clientId(registration.getClientId())
-//                    .clientSecret(registration.getClientSecret())
-//                    .scope("email", "profile")
-//                    .build();
-//        }
-//
-//        if("facebook".equals(provider)) {
-//            OAuth2ClientProperties.Registration registration = clientProperties.getRegistration()
-//                    .get("facebook");
-//
-//            return CommonOAuth2Provider.FACEBOOK.getBuilder(provider)
-//                    .clientId(registration.getClientId())
-//                    .clientSecret(registration.getClientSecret())
-//                    .userInfoUri("https://graph.facebook.com/me?fields=id,name,email,link")
-//                    .scope("email")
-//                    .build();
-//        }
-//
-//
-//        return null;
-//
-//    }
-
 }
