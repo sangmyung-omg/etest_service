414b3ef4a49bd4f1636b95e693e3c616d16a1700
minjoon_lee <minjoon1995@naver.com>
2021-08-02 21:53:53 +0900

    [feature] 프론트로부터 Oauth정보를 제공받을 컨트롤러 만듬

---
+++ eTest/src/main/java/com/tmax/eTest/Auth/controller/ApiController.java
@@ -0,0 +1,73 @@
+package com.tmax.eTest.Auth.controller;
+
+import com.auth0.jwt.JWT;
+import com.auth0.jwt.algorithms.Algorithm;
+import com.tmax.eTest.Auth.dto.CMRespDto;
+import com.tmax.eTest.Auth.dto.SignUpRequestDto;
+import com.tmax.eTest.Auth.model.JwtProperties;
+import com.tmax.eTest.Auth.model.Role;
+import com.tmax.eTest.Auth.repository.UserRepository;
+import com.tmax.eTest.Common.model.user.UserMaster;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.transaction.annotation.Transactional;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Date;
+import java.util.Map;
+import java.util.Optional;
+import java.util.UUID;
+
+@RestController
+@RequiredArgsConstructor
+public class ApiController {
+
+    private final UserRepository userRepository;
+    private final BCryptPasswordEncoder bCryptPasswordEncoder;
+
+    @PostMapping("/login")
+    public CMRespDto<?> jwtCreate(@RequestBody Map<String, Object> data) {
+        System.out.println("로그인 시도");
+        System.out.println(data);
+        Optional<UserMaster> userEntity =
+                userRepository.findByEmail((String) data.get("email"));
+        if (userEntity.isPresent()) {
+            System.out.println("기존에 로그인 했던 유저입니다");
+            String jwtToken = JWT.create()
+                    .withSubject(userEntity.get().getEmail())
+                    .withExpiresAt(new Date(System.currentTimeMillis() + JwtProperties.EXPIRATION_TIME))
+                    .withClaim("userUuid", userEntity.get().getUserUuid())
+                    .withClaim("email", userEntity.get().getEmail())
+                    .sign(Algorithm.HMAC512(JwtProperties.SECRET));
+            return new CMRespDto<>(200, "jwt 반환", jwtToken);
+        }
+
+        System.out.println("처음 로그인 하는 유저입니다");
+        return new CMRespDto<>(200, "회원 가입이 안된 유저",null);
+    }
+    @PostMapping("/signup")
+    @Transactional
+    public CMRespDto<?> signup(@RequestBody SignUpRequestDto signUpRequestDto){
+        UserMaster userMaster = UserMaster.builder()
+            .password(bCryptPasswordEncoder.encode(UUID.randomUUID().toString()))
+            .email(signUpRequestDto.getEmail())
+            .provider(signUpRequestDto.getProvider())
+            .role(Role.USER)
+            .userUuid(UUID.randomUUID().toString())
+            .gender(signUpRequestDto.getGender())
+            .name(signUpRequestDto.getName())
+            .build();
+        userRepository.save(userMaster);
+        String jwtToken = JWT.create()
+                .withSubject(userMaster.getEmail())
+                .withExpiresAt(new Date(System.currentTimeMillis()+ JwtProperties.EXPIRATION_TIME))
+                .withClaim("userUuid", userMaster.getUserUuid())
+                .withClaim("email", userMaster.getEmail())
+                .sign(Algorithm.HMAC512(JwtProperties.SECRET));
+        return new CMRespDto<> (200 , "회원가입 완료",jwtToken);
+    }
+}
+
+
