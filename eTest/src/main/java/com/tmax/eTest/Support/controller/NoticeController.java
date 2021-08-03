075ca989c2fe854d3c5b9b3610ddc217a6d2040b
minjoon_lee <minjoon1995@naver.com>
2021-08-03 18:16:50 +0900

    [feature] noticecontroller로 레포에서 전부 JPA로 긁어오기, creat api 뚫어둠

---
+++ eTest/src/main/java/com/tmax/eTest/Support/controller/NoticeController.java
@@ -0,0 +1,35 @@
+package com.tmax.eTest.Support.controller;
+
+import com.tmax.eTest.Auth.dto.CMRespDto;
+import com.tmax.eTest.Common.model.support.Notice;
+import com.tmax.eTest.Support.Dto.CreateNoticeDto;
+import com.tmax.eTest.Support.repository.NoticeRepository;
+import com.tmax.eTest.Support.service.NoticeService;
+import lombok.AllArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@AllArgsConstructor
+public class NoticeController {
+
+    private final NoticeService noticeService;
+    @Autowired
+    @Qualifier("SU-NoticeRepository")
+    private NoticeRepository noticeRepository;
+
+    @GetMapping("/notice")
+    public CMRespDto<?> findAllNotice() {
+        List<Notice> noticeList =  noticeRepository.findAll();
+        return new CMRespDto<>(200,"가져오기 성공",noticeList);
+    }
+
+    @PostMapping("/notice/create")
+    public CMRespDto<?> createNotice(@RequestBody CreateNoticeDto createNoticeDto) {
+        Long noticeId = noticeService.createNotice(createNoticeDto.getTitle(),createNoticeDto.getDate(),createNoticeDto.getContent());
+        return new CMRespDto<>(200,"만들기 성공",noticeId);
+    }
+}
