9807dd40f1f54265243aa75b0e62cd8213819097
minjoon_lee <minjoon1995@naver.com>
2021-08-04 12:28:24 +0900

    [feature] FAQ,FAQCreateDto생성

---
+++ eTest/src/main/java/com/tmax/eTest/Common/model/support/FAQ.java
@@ -0,0 +1,26 @@
+package com.tmax.eTest.Common.model.support;
+
+import lombok.Data;
+
+import javax.persistence.*;
+
+@Entity
+@Data
+public class FAQ {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    @Column(name = "FAQ_ID")
+    private Long id;
+
+    @Column(name = "FAQ_TITLE")
+    private String title;
+
+    @Column(name = "FAQ_CONTENT")
+    private String content;
+
+    public FAQ(String title, String content) {
+        this.title = title;
+        this.content = content;
+    }
+
+}
---
+++ eTest/src/main/java/com/tmax/eTest/Support/Dto/FAQDto.java
@@ -0,0 +1,10 @@
+package com.tmax.eTest.Support.Dto;
+
+import lombok.Data;
+
+@Data
+public class FAQDto {
+    private String title;
+
+    private String content;
+}
