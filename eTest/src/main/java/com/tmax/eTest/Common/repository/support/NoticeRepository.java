adaae3ae4fbc2b18d9a00f2a44f91e0d907cb71d
minjoon_lee <minjoon1995@naver.com>
2021-08-04 17:04:27 +0900

    별거아님

--- eTest/src/main/java/com/tmax/eTest/Common/repository/support/NoticeRepository.java
+++
@@ -1,7 +0,0 @@
-package com.tmax.eTest.Common.repository.support;
-
-import com.tmax.eTest.Common.model.support.Notice;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface NoticeRepository extends JpaRepository<Notice,Long> {
-}
