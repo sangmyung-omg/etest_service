b645943783f096ff79778175ead60675f7825861
minjoon_lee <minjoon1995@naver.com>
2021-08-04 15:30:12 +0900

    [fix] provider와 providerId가 같은 데이터 가져오도록 쿼리문 생성

--- eTest/src/main/java/com/tmax/eTest/Auth/repository/UserRepository.java
+++ eTest/src/main/java/com/tmax/eTest/Auth/repository/UserRepository.java
@@ -2,8 +2,12 @@ package com.tmax.eTest.Auth.repository;
 
 import com.tmax.eTest.Common.model.user.UserMaster;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
+import java.time.LocalDateTime;
+import java.util.List;
 import java.util.Optional;
 
 @Repository("AU-UserRepository")
@@ -11,5 +15,8 @@ public interface UserRepository extends JpaRepository<UserMaster, Long> {
     Optional<UserMaster> findByEmail(String email);
     Boolean existsByEmail(String email);
     Optional<UserMaster> findByUserUuid(String userUuid);
+    @Query("select u from com.tmax.eTest.Common.model.user.UserMaster u where u.providerId = :providerId and u.provider = :provider")
+    Optional<UserMaster> findByProviderIdAndProvider(@Param("providerId") String providerId,
+                                                     @Param("provider") String provider);
 }
 
