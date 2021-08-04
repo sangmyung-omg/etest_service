0df7b5f919f94a9891dcac978ddc2690a8f5392c
minjoon_lee <minjoon1995@naver.com>
2021-08-04 12:29:27 +0900

    [feature] FAQ controller faq리스트 반환, 생성 성공

---
+++ eTest/src/main/java/com/tmax/eTest/Support/controller/FAQController.java
@@ -0,0 +1,40 @@
+package com.tmax.eTest.Support.controller;
+
+import com.tmax.eTest.Auth.dto.CMRespDto;
+import com.tmax.eTest.Common.model.support.FAQ;
+import com.tmax.eTest.Support.Dto.FAQDto;
+import com.tmax.eTest.Support.repository.FAQRepository;
+import com.tmax.eTest.Support.repository.NoticeRepository;
+import com.tmax.eTest.Support.service.FAQService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+public class FAQController {
+
+    @Autowired
+    @Qualifier("SU-FQARepository")
+    private FAQRepository faqRepository;
+
+    @Autowired
+    private FAQService faqService;
+
+    @PostMapping("/faq/create")
+    public CMRespDto<?> createFAQ(@RequestBody FAQDto faqDto) {
+            Long faqId = faqService.createFAQ(faqDto);
+        return new CMRespDto<>(200,"FAQ 생성 성공",faqId);
+    }
+
+    @GetMapping("/faq")
+    private CMRespDto<?> faq() {
+        List<FAQ> faqList = faqRepository.findAll();
+        return new CMRespDto<>(200,"faqList 반환 성공",faqList);
+    }
+
+}
