49ab8644621977db38f43c646e501a2643527531
youngguen_song <younggeun_song@tmax.co.kr>
2021-08-05 09:53:20 +0900

    [fix] merge ae1-2/dev

--- eTest/src/main/java/com/tmax/eTest/Auth/config/SecurityConfig.java
+++ eTest/src/main/java/com/tmax/eTest/Auth/config/SecurityConfig.java
@@ -5,7 +5,6 @@ import com.tmax.eTest.Auth.repository.UserRepository;
 import com.tmax.eTest.Auth.service.PrincipalDetailsService;
 
 import org.springframework.beans.factory.annotation.Autowired;
-
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -19,12 +18,9 @@ import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
 @Configuration
 @EnableWebSecurity
-@EnableGlobalMethodSecurity(
-        securedEnabled = true,
-        jsr250Enabled = true,
-        prePostEnabled = true)
+@EnableGlobalMethodSecurity(securedEnabled = true, jsr250Enabled = true, prePostEnabled = true)
 
-public class SecurityConfig extends WebSecurityConfigurerAdapter{
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
     @Autowired
     private PrincipalDetailsService principalDetailsService;
@@ -42,6 +38,7 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter{
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
+<<<<<<< HEAD
     http
         .addFilter(corsConfig.corsFilter())
         .csrf().disable() //csrf 토큰
@@ -54,5 +51,18 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter{
                 .antMatchers("/user/**").access("hasRole('ROLE_USER')")
                 .antMatchers("/admin/**").access("hasRole('ROLE_ADMIN')")
                 .anyRequest().permitAll();
+=======
+        http.csrf().disable(); // csrf 토큰
+        http.cors();
+        http.sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS);
+        http.formLogin().disable();
+        http.httpBasic().disable();
+        http.addFilter(new JwtCommonAuthorizationFilter(authenticationManager(), userRepository));
+
+        http.authorizeRequests()
+
+                .antMatchers("/user/**").access("hasRole('ROLE_USER')").antMatchers("/admin/**")
+                .access("hasRole('ROLE_ADMIN')").anyRequest().permitAll();
+>>>>>>> [fix] merge ae1-2/dev
     }
 }
