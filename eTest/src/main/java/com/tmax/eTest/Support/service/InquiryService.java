c8c47779ca2f19e6e5a0047c9d7b10e668b19545
minjoon_lee <minjoon1995@naver.com>
2021-08-06 18:34:17 +0900

    [update] create 함수를 controller에서 서비스로 옮김

--- eTest/src/main/java/com/tmax/eTest/Support/service/InquiryService.java
+++ eTest/src/main/java/com/tmax/eTest/Support/service/InquiryService.java
@@ -1,14 +1,25 @@
 package com.tmax.eTest.Support.service;
 
+import com.tmax.eTest.Auth.dto.PrincipalDetails;
+import com.tmax.eTest.Auth.repository.UserRepository;
 import com.tmax.eTest.Common.model.support.Inquiry;
 import com.tmax.eTest.Common.model.support.Inquiry_file;
+import com.tmax.eTest.Common.model.user.UserMaster;
 import com.tmax.eTest.Support.dto.CreateInquiryDto;
+import com.tmax.eTest.Support.repository.InquiryFileRepository;
 import com.tmax.eTest.Support.repository.InquiryRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
 import java.util.List;
+import java.util.Optional;
 import java.util.UUID;
 
 @Service
@@ -17,36 +28,51 @@ public class InquiryService {
     @Autowired
     @Qualifier("SU-InquiryRepository")
     InquiryRepository inquiryRepository;
+    @Autowired
+    @Qualifier("SU-InquiryFileRepository")
+    InquiryFileRepository inquiryFileRepository;
+
+    @Autowired
+    @Qualifier("AU-UserRepository")
+    UserRepository userRepository;
+
+    @Value("${file.path}")
+    private String uploadFolder;
 
     @Transactional
-    public Long createInquiry(CreateInquiryDto createInquiryDto) {
-        System.out.println("inquiry service 돌입");
-        System.out.println(createInquiryDto);
+    public Inquiry createInquiry(CreateInquiryDto createInquiryDto, PrincipalDetails principalDetails) {
+        List<MultipartFile> fileList = createInquiryDto.getFileList();
+        Optional<UserMaster> userMaster_temp = userRepository.findByEmail(principalDetails.getEmail());
+        UserMaster userMasterEntity= userMaster_temp.get();
+        Inquiry inquiry =
+                Inquiry.builder()
+                        .userMaster(userMasterEntity)
+                        .content(createInquiryDto.getContent())
+                        .status(createInquiryDto.getStatus())
+                        .date(createInquiryDto.getDate())
+                        .title(createInquiryDto.getTitle())
+                        .type(createInquiryDto.getType())
+                        .build();
+        inquiryRepository.save(inquiry);
+        for(int i=0; i<createInquiryDto.getFileList().size(); i++){
+            String fileName = UUID.randomUUID().toString() + "_" + createInquiryDto.getFileList().get(i).getOriginalFilename();
+            Path imageFilePath = Paths.get(uploadFolder + fileName);
+            Inquiry_file inquiry_file = Inquiry_file.builder()
+                    .url(fileName)
+                    .type(createInquiryDto.getFileList().get(i).getContentType())
+                    .inquiry(inquiry)
+                    .build();
+            try {
 
-
-        List<Inquiry_file> url = createInquiryDto.getInquiry_file();
-        for(Inquiry_file element : url){                                    //리스트 원소 돌면서 각 Url에 UUID를 붙여줘서 고유하게 만들어준다.
-            String element_url = element.getUrl();                          // 첨부파일 -> 질문 -> 유저 조회하는게 맞는가? 첨부파일 -> 유저로 조회하는게 맞는가?
-            element.setUrl(UUID.randomUUID().toString() + element_url);
-            System.out.println(element.getUrl());
-
+                Files.write(imageFilePath, createInquiryDto.getFileList().get(i).getBytes());
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+            inquiryFileRepository.save(inquiry_file);
         }
 
-        createInquiryDto.setInquiry_file(url);
 
-
-
-        Inquiry inquiry = Inquiry.builder()
-                .answer(createInquiryDto.getAnswer())
-                .content(createInquiryDto.getContent())
-                .date(createInquiryDto.getDate())
-                .status(createInquiryDto.getStatus())
-                .title(createInquiryDto.getTitle())
-                .type(createInquiryDto.getType())
-                .inquiry_file(createInquiryDto.getInquiry_file())
-                .build();
-        System.out.println("inquiry.getId() 결과 :" + inquiry.getId().toString() );
-        return inquiry.getId();
+        return inquiry;
     }
 
 }
