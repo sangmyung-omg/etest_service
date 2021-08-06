ef0f686688f693ad0703c74fdb4648b593100d77
이진형 <jinhyung_lee@tmax.co.kr>
2021-08-06 10:17:42 +0900

    [feat] mini,self list

--- eTest/src/main/java/com/tmax/eTest/TestStudio/controller/MiniController.java
+++ eTest/src/main/java/com/tmax/eTest/TestStudio/controller/MiniController.java
@@ -3,17 +3,20 @@ package com.tmax.eTest.TestStudio.controller;
 import java.util.List;
 import java.util.stream.Collectors;
 
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.util.ObjectUtils;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 import com.tmax.eTest.Common.model.problem.TestProblem;
-import com.tmax.eTest.Common.repository.problem.TestProblemRepo;
 import com.tmax.eTest.TestStudio.dto.MiniProblemListDTO;
+import com.tmax.eTest.TestStudio.repository.MiniRepository;
 
 import lombok.RequiredArgsConstructor;
 
@@ -22,14 +25,20 @@ import lombok.RequiredArgsConstructor;
 @RequiredArgsConstructor
 public class MiniController {
 
-	private final TestProblemRepo testProblemRepo;
+	private final MiniRepository miniRepository;
 
 	@GetMapping("test-studio/problems/mini")
 	@ResponseBody
-	public ResponseEntity<List<MiniProblemListDTO>> PartList() {
+	public ResponseEntity<List<MiniProblemListDTO>> MiniProblemList(
+			@RequestParam(value="partId", required=false) Integer partId,
+			@RequestParam(value="keyword", required=false) String keyword,
+			@RequestParam(value="order", required=false) String order,
+			@RequestParam(value="orderOption",defaultValue="ascending") String orderOption,
+			@RequestParam(value="pageNo", defaultValue="0") int pageNo,
+			@RequestParam(value="pageCount",defaultValue="20") int pageCount) {
 		try {
-			List<TestProblem> query = testProblemRepo.findAll();
-			List<MiniProblemListDTO> body = query.stream().map(m -> new MiniProblemListDTO(
+			Page<TestProblem> query = miniRepository.searchMiniProblems(partId, keyword, order, orderOption, PageRequest.of(pageNo, pageCount));
+			List<MiniProblemListDTO> body = query.getContent().stream().map(m -> new MiniProblemListDTO(
 					m.getProbID(),
 					ObjectUtils.isEmpty(m.getPart()) ? null : m.getPart().getPartName(),
 					m.getProblem().getDifficulty(),
