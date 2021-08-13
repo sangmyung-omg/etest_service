6df6cf53c232e56b66abc1bbcee0eff4957a7570
minjoon_lee <minjoon1995@naver.com>
2021-08-13 16:17:49 +0900

    [feature] 자가진단기록받아오는 컨트롤러 추가

---
+++ eTest/src/main/java/com/tmax/eTest/MyPage/controller/MyPageDiagnosisController.java
@@ -0,0 +1,27 @@
+package com.tmax.eTest.MyPage.controller;
+
+import com.tmax.eTest.Auth.dto.CMRespDto;
+import com.tmax.eTest.Auth.dto.PrincipalDetails;
+import com.tmax.eTest.Common.model.report.DiagnosisReport;
+import com.tmax.eTest.MyPage.service.MyPageDiagnosisService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import java.util.List;
+import java.util.Optional;
+
+@RestController
+@RequestMapping("/user")
+public class MyPageDiagnosisController {
+    @Autowired
+    MyPageDiagnosisService diagnosisService;
+
+    @GetMapping("/myPage/getDiagnosisList")
+    public CMRespDto<?> getDiagnosisList(@AuthenticationPrincipal PrincipalDetails principalDetails) {
+        List<DiagnosisReport> diagnosisReport = diagnosisService.getDiagnosisList(principalDetails.getUserUuid());
+//        Optional<DiagnosisReport> diagnosisReport = diagnosisService.getDiagnosisList("022ed3de-08b3-4874-95b7-0a557581d85b");
+        return new CMRespDto<>(200,"test",diagnosisReport);
+    }
+}
