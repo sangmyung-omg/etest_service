351299ef2f8bced51e2b18b77db4a39bab5a7af8
minjoon_lee <minjoon1995@naver.com>
2021-08-20 14:08:57 +0900

    [Update] 자가진단 히스토리만 리턴하는 sql 생성

--- eTest/src/main/java/com/tmax/eTest/MyPage/repository/DiagnosisReportRepo.java
+++ eTest/src/main/java/com/tmax/eTest/MyPage/repository/DiagnosisReportRepo.java
@@ -2,7 +2,9 @@ package com.tmax.eTest.MyPage.repository;
 
 import com.tmax.eTest.Common.model.report.DiagnosisReport;
 import com.tmax.eTest.Common.model.report.DiagnosisReportKey;
+import com.tmax.eTest.MyPage.dto.DiagnosisReportHistoryDTO;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 import java.util.List;
 import java.util.Optional;
@@ -10,5 +12,9 @@ import java.util.Optional;
 @Repository("MY-DiagnosisReportRepo")
 public interface DiagnosisReportRepo extends JpaRepository<DiagnosisReport, DiagnosisReportKey> {
 
-    List<DiagnosisReport> findAllByUserUuid(String UserUuid);
+    List<DiagnosisReport> findByUserUuid(String UserUuid);
+
+    @Query(value = "select * FROM Diagnosis_Report WHERE USER_UUID = :USER_UUID", nativeQuery = true)
+    List<DiagnosisReportHistoryDTO> findHistoryByUserUuid(String UserUuid);
+
 }
