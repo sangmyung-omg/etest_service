abcbcabf7b91fb6e888129252479274a50642ed1
오성돈 <sungdon_oj@tmax.co.kr>
2021-08-20 13:42:54 +0900

    [feat] DiagnosisRecord Dummy 데이터 조정.

--- eTest/src/main/java/com/tmax/eTest/Report/dto/DiagnosisRecordDTO.java
+++ eTest/src/main/java/com/tmax/eTest/Report/dto/DiagnosisRecordDTO.java
@@ -18,66 +18,84 @@ public class DiagnosisRecordDTO {
 	int giScore, giPercentage;
 
 	// 점수 관련
-	Map<String, Integer> riskScoreInfo;
-	Map<String, Integer> investScoreInfo;
-	Map<String, Integer> knowledgeScoreInfo;
+	Map<String, Object> scoreInfo = new HashMap<>();
 	
 	// Comment 관련
-	Map<String, String> riskCommentInfo;
-	Map<String, String> investCommentInfo;
-	Map<String, String> knowledgeCommentInfo;
+	Map<String, String> mainCommentInfo = new HashMap<>();
+	Map<String, Object> detailCommentInfo = new HashMap<>();
 	
 	// 문제 정보
-	Map<String, Integer> problemCorrectInfo;
-	List<List<String>> problemHighLevelInfo;
-	List<List<String>> problemMiddleLevelInfo;
-	List<List<String>> problemLowLevelInfo;
+	Map<String, Object> problemCorrectInfo = new HashMap<>();
+	List<List<String>> problemHighLevelInfo = new ArrayList<>();
+	List<List<String>> problemMiddleLevelInfo = new ArrayList<>();
+	List<List<String>> problemLowLevelInfo = new ArrayList<>();
 	
 	public boolean initForDummy() {
 		giScore = 85;
 		giPercentage = 22;
 		
+		Map<String, Integer> riskScoreInfo = new HashMap<>();
+		Map<String, Integer> investScoreInfo = new HashMap<>();
+		Map<String, Integer> knowledgeScoreInfo = new HashMap<>();
+		
 		riskScoreInfo.put("score", 71);
 		riskScoreInfo.put("scoreUpper10", 89);
-		riskScoreInfo.put("profile", 15);
-		riskScoreInfo.put("tracing", 9);
-		riskScoreInfo.put("tracingLevel", 4);
-		riskScoreInfo.put("tracingCapa", 5);
-		
-		riskCommentInfo.put("main", "전형적인 방어형 투자자");
-		riskCommentInfo.put("투자위험에 대한 자세", "리스크 회피형");
-		riskCommentInfo.put("투자 방법", "분산 투자형");
+		riskScoreInfo.put("scoreAvg", 65);
+		riskScoreInfo.put("percentage", 20);
+		//riskScoreInfo.put("profile", 15);
+		//riskScoreInfo.put("tracing", 9);
+		//riskScoreInfo.put("tracingLevel", 4);
+		//riskScoreInfo.put("tracingCapa", 5);
 		
 		investScoreInfo.put("score", 85);
 		investScoreInfo.put("scoreUpper10", 83);
-		investScoreInfo.put("profile", 40);
-		investScoreInfo.put("tracing", 45);
-		
-		investCommentInfo.put("main", "투자원칙은 확고하지 않지만 이성적이며 행동편향은 보통인 투자자");
-		investCommentInfo.put("행동편향 정도", "이성적인 투자자");
-		investCommentInfo.put("투자원칙 정도", "투자원칙이 확고하지 않은 투자자");
+		investScoreInfo.put("scoreAvg", 78);
+		investScoreInfo.put("percentage", 8);
+		//investScoreInfo.put("profile", 40);
+		//investScoreInfo.put("tracing", 45);
 		
 		knowledgeScoreInfo.put("score", 89);
 		knowledgeScoreInfo.put("scoreUpper10", 75);
-		knowledgeScoreInfo.put("common", 22);
-		knowledgeScoreInfo.put("type", 21);
-		knowledgeScoreInfo.put("change", 22);
-		knowledgeScoreInfo.put("sell", 24);
+		knowledgeScoreInfo.put("scoreAvg", 78);
+		knowledgeScoreInfo.put("percentage", 8);
+//		knowledgeScoreInfo.put("common", 22);
+//		knowledgeScoreInfo.put("type", 21);
+//		knowledgeScoreInfo.put("change", 22);
+//		knowledgeScoreInfo.put("sell", 24);
 		
-		knowledgeCommentInfo.put("main", "우수");
+		scoreInfo.put("risk", riskScoreInfo);
+		scoreInfo.put("invest", investScoreInfo);
+		scoreInfo.put("knowledge", knowledgeScoreInfo);
+		
+		
+		mainCommentInfo.put("risk", "전형적인 방어형 투자자");
+		mainCommentInfo.put("invest", "투자원칙은 확고하지 않지만 이성적이며 행동편향은 보통인 투자자");
+		mainCommentInfo.put("knowledge", "우수");
+		
+		Map<String, String> riskCommentInfo = new HashMap<>();
+		Map<String, String> investCommentInfo = new HashMap<>();
+		Map<String, String> knowledgeCommentInfo = new HashMap<>();
+		
+		riskCommentInfo.put("투자위험에 대한 자세", "리스크 회피형");
+		riskCommentInfo.put("투자 방법", "분산 투자형");
+		
+		investCommentInfo.put("행동편향 정도", "이성적인 투자자");
+		investCommentInfo.put("투자원칙 정도", "투자원칙이 확고하지 않은 투자자");
+		
 		knowledgeCommentInfo.put("투자기초", "우수");
 		knowledgeCommentInfo.put("종목고르기", "양호");
 		knowledgeCommentInfo.put("가격변동특징", "우수");
 		knowledgeCommentInfo.put("매매방법", "우수");
 		
-		problemCorrectInfo.put("allCorr", 12);
-		problemCorrectInfo.put("allWrong", 9);
-		problemCorrectInfo.put("highCorr", 2);
-		problemCorrectInfo.put("highWrong", 1);
-		problemCorrectInfo.put("middleCorr", 5);
-		problemCorrectInfo.put("middleWrong", 2);
-		problemCorrectInfo.put("lowCorr", 2);
-		problemCorrectInfo.put("lowWrong", 0);
+		detailCommentInfo.put("risk", riskCommentInfo);
+		detailCommentInfo.put("invest", investCommentInfo);
+		detailCommentInfo.put("knowledge", knowledgeCommentInfo);
+		
+		problemCorrectInfo.put("allCorr", 9);
+		problemCorrectInfo.put("allWrong", 3);
+		problemCorrectInfo.put("high", "2/3");
+		problemCorrectInfo.put("middle", "5/7");
+		problemCorrectInfo.put("low", "2/2");
 
 		List<String> probInfo = new ArrayList<>();
 		probInfo.add("문제 UUID");
