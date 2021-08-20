5bf199c4e58397170ab926affea9f2ca7843054d
minjoon_lee <minjoon1995@naver.com>
2021-08-20 14:09:20 +0900

    [feature] 컨트롤러에 자가진단 지우는 기능 껍데기만 생성

--- eTest/src/main/java/com/tmax/eTest/MyPage/controller/MyPageDiagnosisController.java
+++ eTest/src/main/java/com/tmax/eTest/MyPage/controller/MyPageDiagnosisController.java
@@ -3,10 +3,12 @@ package com.tmax.eTest.MyPage.controller;
 import com.tmax.eTest.Auth.dto.CMRespDto;
 import com.tmax.eTest.Auth.dto.PrincipalDetails;
 import com.tmax.eTest.Common.model.report.DiagnosisReport;
+import com.tmax.eTest.MyPage.dto.DeleteDiagnosisReportDTO;
 import com.tmax.eTest.MyPage.service.MyPageDiagnosisService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import java.util.List;
@@ -22,6 +24,14 @@ public class MyPageDiagnosisController {
     public CMRespDto<?> getDiagnosisList(@AuthenticationPrincipal PrincipalDetails principalDetails) {
         List<DiagnosisReport> diagnosisReport = diagnosisService.getDiagnosisList(principalDetails.getUserUuid());
 //        Optional<DiagnosisReport> diagnosisReport = diagnosisService.getDiagnosisList("022ed3de-08b3-4874-95b7-0a557581d85b");
-        return new CMRespDto<>(200,"test",diagnosisReport);
+        return new CMRespDto<>(200, "test", diagnosisReport);
     }
+
+    @PostMapping("/myPage/deleteDiagnosisList")
+    public CMRespDto<?> deleteDiagnosisList(@AuthenticationPrincipal PrincipalDetails principalDetails, DeleteDiagnosisReportDTO deleteDiagnosisReportDTO) {
+        return null;
+    }
+
+
+
 }
