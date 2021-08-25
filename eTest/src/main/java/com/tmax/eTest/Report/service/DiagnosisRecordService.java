f27ccd7f8e3d6a366ce73184cbe456c751c4256d
오성돈 <sungdon_oj@tmax.co.kr>
2021-08-25 15:51:35 +0900

    [feat] 자가진단 더미 재구성. (Main 과 Detail의 분류)

--- eTest/src/main/java/com/tmax/eTest/Report/service/DiagnosisRecordService.java
+++ eTest/src/main/java/com/tmax/eTest/Report/service/DiagnosisRecordService.java
@@ -1,4 +1,4 @@
- package com.tmax.eTest.Report.service;
+package com.tmax.eTest.Report.service;
 
 import java.sql.Timestamp;
 import java.time.LocalDateTime;
@@ -8,6 +8,7 @@ import java.util.Comparator;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
+import java.util.Optional;
 import java.util.UUID;
 
 import org.slf4j.Logger;
@@ -18,7 +19,8 @@ import org.springframework.stereotype.Service;
 
 import com.tmax.eTest.Common.model.problem.Problem;
 import com.tmax.eTest.Contents.repository.ProblemRepository;
-import com.tmax.eTest.Report.dto.DiagnosisRecordDTO;
+import com.tmax.eTest.Report.dto.DiagnosisRecordDetailDTO;
+import com.tmax.eTest.Report.dto.DiagnosisRecordMainDTO;
 import com.tmax.eTest.Report.dto.DiagnosisResultDTO;
 import com.tmax.eTest.Report.dto.PartUnderstandingDTO;
 import com.tmax.eTest.Report.dto.lrs.GetStatementInfoDTO;
@@ -34,6 +36,7 @@ import com.tmax.eTest.Report.util.StateAndProbProcess;
 import com.tmax.eTest.Report.util.TritonAPIManager;
 import com.tmax.eTest.Report.util.UKScoreCalculator;
 import com.tmax.eTest.Common.model.report.DiagnosisReport;
+import com.tmax.eTest.Common.model.report.DiagnosisReportKey;
 import com.tmax.eTest.Common.model.uk.UkMaster;
 import com.tmax.eTest.Common.repository.report.DiagnosisReportRepo;
 import com.tmax.eTest.Test.repository.UserKnowledgeRepository;
@@ -45,14 +48,14 @@ public class DiagnosisRecordService {
 	LRSAPIManager lrsAPIManager;
 	@Autowired
 	TritonAPIManager tritonAPIManager;
-	
+
 	@Autowired
 	ProblemRepository problemRepo;
 	@Autowired
 	UserKnowledgeRepository userKnowledgeRepo;
 	@Autowired
 	DiagnosisReportRepo diagnosisReportRepo;
-	
+
 	@Autowired
 	StateAndProbProcess stateAndProbProcess;
 	@Autowired
@@ -63,67 +66,90 @@ public class DiagnosisRecordService {
 	SelfDiagnosisComment commentGenerator;
 	@Autowired
 	SNDCalculator sndCalculator;
-	
+
 	private final Logger logger = LoggerFactory.getLogger(this.getClass().getSimpleName());
-	
-	public DiagnosisRecordDTO getDiagnosisRecord(
-			String userId, 
-			String probSetId) throws Exception
-	{
-		DiagnosisRecordDTO result = new DiagnosisRecordDTO();
-		
-		List<StatementDTO> diagStatement = getDiagnosisProbInfo(userId, null);
-		lrsAPIManager.saveStatementList(diagStatement);
-		
-		// 1. 해당 유저의 문제 푼 정보 수집.
-		
-//		setProbInfoInRecordDTO(result, diagStatement);
+
+	public DiagnosisRecordMainDTO getDiagnosisRecordMain(String userId, String probSetId) throws Exception {
+		DiagnosisRecordMainDTO result = new DiagnosisRecordMainDTO();
+
+//		DiagnosisReportKey key = new DiagnosisReportKey();
+//		key.setDiagnosisId(probSetId);
 //		
+//		Optional<DiagnosisReport> report = diagnosisReportRepo.findById(key);
+//		
+//		if(report.isPresent())
+//		{
+//			
+//		}
+//		else
+//			throw new ReportBadRequestException("Problem Set Id is unavailable. "+probSetId);
+
 		result.initForDummy();
-		
+
 		return result;
 	}
 	
-	// LRS에서 푼 문제 정보를 모아, 관련 정보 생성. (난이도별 문제 맞은 갯수, 해당 문제 정보 등)
-	private void setProbInfoInRecordDTO(DiagnosisRecordDTO output, List<StatementDTO> infos)
-	{
-		
-		// probId, isCorr
-		Map<Integer, Integer> probCorrInfo = new HashMap<>();
-		
-		for(StatementDTO info : infos)
-		{
-			if(info.getSourceType().equals("diagnosis"))
-			{
-				try
-				{
-					int probId = Integer.getInteger(info.getSourceId());
-					probCorrInfo.put(probId, info.getIsCorrect());
-					
-				}
-				catch(Exception e)
-				{
-					logger.info("Integer decode fail in setProbInfoInRecordDTO "+info.toString());
-				}
-			}
-		}
-		
-		List<Problem> diagProbs = problemRepo.findAllById(probCorrInfo.keySet());
-		output.pushProblemList(diagProbs, probCorrInfo);
-		
+	public DiagnosisRecordDetailDTO getDiagnosisRecordDetail(
+			String userId, 
+			String probSetId,
+			String partName) throws Exception {
+		DiagnosisRecordDetailDTO result = new DiagnosisRecordDetailDTO();
+
+//		DiagnosisReportKey key = new DiagnosisReportKey();
+//		key.setDiagnosisId(probSetId);
+//		
+//		Optional<DiagnosisReport> report = diagnosisReportRepo.findById(key);
+//		
+//		if(report.isPresent())
+//		{
+//			
+//		}
+//		else
+//			throw new ReportBadRequestException("Problem Set Id is unavailable. "+probSetId);
+
+		result.initForDummy();
+
+		return result;
 	}
 	
-	private List<StatementDTO> getDiagnosisProbInfo(String userId, String probSetId) throws Exception
-	{
-		
+
+	// LRS에서 푼 문제 정보를 모아, 관련 정보 생성. (난이도별 문제 맞은 갯수, 해당 문제 정보 등)
+//	private void setProbInfoInRecordDTO(DiagnosisRecordMainDTO output, List<StatementDTO> infos) {
+//
+//		// probId, isCorr
+//		Map<Integer, Integer> probCorrInfo = new HashMap<>();
+//		List<Integer> probIds = new ArrayList<>();
+//
+//		for (StatementDTO info : infos) {
+//			if (info.getSourceType().equals("diagnosis")) {
+//				try {
+//					int probId = Integer.parseInt(info.getSourceId());
+//					probCorrInfo.put(probId, info.getIsCorrect());
+//				} catch (Exception e) {
+//					logger.info("Integer decode fail in setProbInfoInRecordDTO " + info.getSourceId());
+//				}
+//			}
+//		}
+//
+//		// 왜 안되냐 ㅡㅡ;;
+//		List<Problem> diagProbs = problemRepo.findAllById(probCorrInfo.keySet());
+//		logger.info(diagProbs.size() + " "+probCorrInfo.keySet().toString());
+//		for (Problem prob : diagProbs)
+//			logger.info(prob.getProbID() + "");
+//		//output.pushProblemList(diagProbs, probCorrInfo);
+//		logger.info(probIds.toString());
+//	}
+
+	private List<StatementDTO> getDiagnosisProbInfo(String userId, String probSetId) throws Exception {
+
 		GetStatementInfoDTO input = new GetStatementInfoDTO();
 		input.pushUserId(userId);
-		if(probSetId != null)
+		if (probSetId != null)
 			input.pushExtensionStr(probSetId);
 		input.pushActionType("submit");
 		List<StatementDTO> result = lrsAPIManager.getStatementList(input);
-		
+
 		return result;
-		
+
 	}
 }
