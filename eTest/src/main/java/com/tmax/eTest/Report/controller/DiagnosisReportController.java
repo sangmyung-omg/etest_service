ba5b3f1b92fda07eecd0a538b56a944aa8339875
오성돈 <sungdon_oj@tmax.co.kr>
2021-09-02 09:25:29 +0000

    [feat] 자가진단 비인가 루트 추가.

--- eTest/src/main/java/com/tmax/eTest/Report/controller/DiagnosisReportController.java
+++ eTest/src/main/java/com/tmax/eTest/Report/controller/DiagnosisReportController.java
@@ -1,6 +1,10 @@
 package com.tmax.eTest.Report.controller;
 
 
+import java.util.Map;
+
+import javax.servlet.http.HttpServletRequest;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -9,6 +13,7 @@ import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.tmax.eTest.Auth.jwt.JwtTokenUtil;
 import com.tmax.eTest.Report.dto.DiagnosisRecordDetailDTO;
 import com.tmax.eTest.Report.dto.DiagnosisRecordMainDTO;
 import com.tmax.eTest.Report.service.DiagnosisDetailRecordService;
@@ -17,6 +22,9 @@ import com.tmax.eTest.Report.service.DiagnosisReportService;
 import com.tmax.eTest.Report.service.DiagnosisVideoService;
 import com.tmax.eTest.Test.service.UserInfoService;
 
+import lombok.extern.log4j.Log4j2;
+
+@Log4j2
 @RestController
 @CrossOrigin(origins = "*")
 @RequestMapping(path = "/report/diagnosis")
@@ -24,69 +32,87 @@ public class DiagnosisReportController {
 	
 	@Autowired
 	DiagnosisReportService selfDiagnosisReportService;
-	
 	@Autowired
 	DiagnosisMainRecordService diagnosisMainRecordService;
 	@Autowired
 	DiagnosisDetailRecordService diagnosisDetailRecordService;
-	
-	
 	@Autowired
 	DiagnosisVideoService diagnosisVideoService;
-	
 	@Autowired
 	UserInfoService userService;
 	
+	@Autowired
+	JwtTokenUtil jwtTokenUtil;
+	
 
-	@PutMapping(value="/saveResult/{id}/{probSetId}", produces = "application/json; charset=utf-8")
+	@PutMapping(value="/saveResult/{probSetId}", produces = "application/json; charset=utf-8")
 	public boolean diagnosisResult(
-			@PathVariable("id") String id,
+			HttpServletRequest request,
 			@PathVariable("probSetId") String probSetId) throws Exception{
 		
-		// Saving user ID of not-logged-in users - by S.M.
-		//String queryResult = userService.updateUserInfo(id);
-		// ------------------------------------------------
+		String id = getUserIDFromRequest(request);
 		
 		boolean output = selfDiagnosisReportService.saveDiagnosisResult(id, probSetId);
 		
 		return output;
 	}
 	
-	@GetMapping(value="/record/{id}/{probSetId}", produces = "application/json; charset=utf-8")
+	@GetMapping(value="/record/{probSetId}", produces = "application/json; charset=utf-8")
 	public DiagnosisRecordMainDTO diagnosisRecordMain(
-			@PathVariable("id") String id,
+			HttpServletRequest request,
 			@PathVariable("probSetId") String probSetId) throws Exception{
 		
-		// Saving user ID of not-logged-in users - by S.M.
-		//String queryResult = userService.updateUserInfo(id);
-		// ------------------------------------------------
+
+		String id = getUserIDFromRequest(request);
 		
 		DiagnosisRecordMainDTO output = diagnosisMainRecordService.getDiagnosisRecordMain(id, probSetId);
 		
 		return output;
 	}
 	
-	@GetMapping(value="/record/{id}/{probSetId}/{partName}", produces = "application/json; charset=utf-8")
+	@GetMapping(value="/record/{probSetId}/{partName}", produces = "application/json; charset=utf-8")
 	public DiagnosisRecordDetailDTO diagnosisRecordDetail(
-			@PathVariable("id") String id,
+			HttpServletRequest request,
 			@PathVariable("probSetId") String probSetId,
 			@PathVariable("partName") String partName) throws Exception{
 		
-		// Saving user ID of not-logged-in users - by S.M.
-		//String queryResult = userService.updateUserInfo(id);
-		// ------------------------------------------------
+		String id = getUserIDFromRequest(request);
 		
 		DiagnosisRecordDetailDTO output = diagnosisDetailRecordService.getDiagnosisRecordDetail(id, probSetId, partName);
 		
 		return output;
 	}
 	
-	@PutMapping(value="/bookmark/{id}/{videoId}/{isBookmarkOn}", produces = "application/json; charset=utf-8")
+	@PutMapping(value="/bookmark/{videoId}/{isBookmarkOn}", produces = "application/json; charset=utf-8")
 	public boolean setVideoBookmark(
-			@PathVariable("id") String userId,
+			HttpServletRequest request,
 			@PathVariable("videoId") Long videoId,
 			@PathVariable("isBookmarkOn") Boolean isBookmarkOn) throws Exception{
+
+		String id = getUserIDFromRequest(request);
 		
-		return diagnosisVideoService.setVideoBookmark(userId, videoId, isBookmarkOn);
+		return diagnosisVideoService.setVideoBookmark(id, videoId, isBookmarkOn);
+	}
+	
+	
+	public String getUserIDFromRequest(HttpServletRequest request)
+	{
+		String result = null;
+		String header = request.getHeader("Authorization");
+		
+		if (header == null) {																// 비회원일 때 토큰 null
+			log.info("header.Authorization is null. No token given.");
+		} else {																			// 회원이면 토큰 파싱하여 유저 아이디 꺼냄
+			String token = request.getHeader("Authorization").replace("Bearer ","");
+			try {
+				Map<String, Object> parseInfo = jwtTokenUtil.getUserParseInfo(token);
+				result = parseInfo.get("userUuid").toString();
+				log.info("User UUID from header token : " + result);
+			} catch (Exception e) {
+				log.info("error : cannot parse jwt token, " + e.getMessage());
+			}
+		}
+		
+		return result;
 	}
 }
