0af56a8fe87754a0c2f34d457f5eab2610ce677d
이민준 <minjoon_lee@tmax.co.kr>
2021-09-08 06:16:37 +0000

    [fix] security config filter add master submaster filter

--- eTest/src/main/java/com/tmax/eTest/Auth/dto/Role.java
+++ eTest/src/main/java/com/tmax/eTest/Auth/dto/Role.java
@@ -6,9 +6,12 @@ import lombok.RequiredArgsConstructor;
 @Getter
 @RequiredArgsConstructor
 public enum Role {
+
     USER("ROLE_USER", "일반 사용자"),
-    ADMIN("ROLE_ADMIN", "관리자");
+    MASTER("ROLE_MASTER", "관리자"),
+    SUB_MASTER("ROLE_SUB_MASTER", "서브 관리자");
 
     private final String key;
     private final String title;
+
 }
