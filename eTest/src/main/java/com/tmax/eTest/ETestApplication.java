dea85a23366b0990c8ee4e92bf4a11a2d6232b33
minjoon_lee <minjoon1995@naver.com>
2021-09-10 16:34:58 +0900

    [fix] ETestApllication에서 time zone 서울로 설정

--- eTest/src/main/java/com/tmax/eTest/ETestApplication.java
+++ eTest/src/main/java/com/tmax/eTest/ETestApplication.java
@@ -5,11 +5,18 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.context.annotation.EnableAspectJAutoProxy;
 
+import javax.annotation.PostConstruct;
+import java.util.Date;
+import java.util.TimeZone;
+
 @SpringBootApplication
 @EnableCaching
 @EnableAspectJAutoProxy
 public class ETestApplication {
-
+	@PostConstruct
+	public void setTimeZone(){
+		TimeZone.setDefault(TimeZone.getTimeZone("Asiz/Seoul"));
+	}
 	public static void main(String[] args) {
 		SpringApplication.run(ETestApplication.class, args);
 	}
