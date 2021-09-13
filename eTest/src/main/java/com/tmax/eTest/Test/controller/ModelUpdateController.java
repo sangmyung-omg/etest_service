93cd190d383c26231ec943cee33c340d710993d8
이상명 <sangmyung_lee@tmax.co.kr>
2021-09-13 06:39:46 +0000

    feat: Add ukRel api (Ongoing) and Modify etest swagger

---
+++ eTest/src/main/java/com/tmax/eTest/Test/controller/ModelUpdateController.java
@@ -0,0 +1,31 @@
+package com.tmax.eTest.Test.controller;
+
+import com.tmax.eTest.Test.config.TestPathConstant;
+import com.tmax.eTest.Test.dto.UkRelOutputDTO;
+import com.tmax.eTest.Test.service.ModelUpdateService;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
+@RestController
+@CrossOrigin(origins = "*", allowedHeaders = "*")
+@RequestMapping(path = "/model")
+public class ModelUpdateController {
+    
+    @Autowired
+    ModelUpdateService modelService;
+
+    @GetMapping(value = "/ukRel", produces = "application/json; charset=utf-8")
+    public ResponseEntity<Object> getUkRelations() {
+        UkRelOutputDTO result = modelService.getUkRelInfo();
+        return new ResponseEntity<>(result, HttpStatus.OK);
+    }
+}
---
+++ eTest/src/main/java/com/tmax/eTest/Test/dto/UkRelOutputDTO.java
@@ -0,0 +1,8 @@
+package com.tmax.eTest.Test.dto;
+
+import lombok.Data;
+
+@Data
+public class UkRelOutputDTO {
+    
+}
