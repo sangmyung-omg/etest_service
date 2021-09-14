c7547b318b6b14b50f1c185497f8ec5a4688f451
이상명 <sangmyung_lee@tmax.co.kr>
2021-09-14 04:13:42 +0000

    chore: Modify build.gradle and app.java to add config for WAR construction

--- eTest/src/main/java/com/tmax/eTest/ETestApplication.java
+++ eTest/src/main/java/com/tmax/eTest/ETestApplication.java
@@ -2,6 +2,8 @@ package com.tmax.eTest;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.context.annotation.EnableAspectJAutoProxy;
 
@@ -12,11 +14,17 @@ import java.util.TimeZone;
 @SpringBootApplication
 @EnableCaching
 @EnableAspectJAutoProxy
-public class ETestApplication {
+public class ETestApplication extends SpringBootServletInitializer {
 	@PostConstruct
 	public void setTimeZone(){
 		TimeZone.setDefault(TimeZone.getTimeZone("Asia/Seoul"));
 	}
+
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(ETestApplication.class);
+	}
+	
 	public static void main(String[] args) {
 		SpringApplication.run(ETestApplication.class, args);
 	}
