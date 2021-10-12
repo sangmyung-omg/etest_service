4d35193962a891d16b0215ae4951bab783a0fc49
youngguen_song <younggeun_song@tmax.co.kr>
2021-10-12 14:36:18 +0900

    rebase master

--- eTest/src/main/java/com/tmax/eTest/Auth/service/AuthService.java
+++ eTest/src/main/java/com/tmax/eTest/Auth/service/AuthService.java
@@ -1,34 +1,40 @@
 package com.tmax.eTest.Auth.service;
 
-import com.tmax.eTest.Auth.controller.AuthController;
-import com.tmax.eTest.Auth.dto.*;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.Optional;
+import java.util.TimeZone;
+import java.util.UUID;
+
+import com.tmax.eTest.Auth.dto.AuthProvider;
+import com.tmax.eTest.Auth.dto.CMRespDto;
+import com.tmax.eTest.Auth.dto.ModifyUserInfoDto;
+import com.tmax.eTest.Auth.dto.PrincipalDetails;
+import com.tmax.eTest.Auth.dto.Role;
+import com.tmax.eTest.Auth.dto.SignUpRequestDto;
 import com.tmax.eTest.Auth.jwt.JwtTokenUtil;
 import com.tmax.eTest.Auth.repository.UserRepository;
 import com.tmax.eTest.Common.model.user.UserMaster;
 import com.tmax.eTest.LRS.dto.StatementDTO;
 import com.tmax.eTest.LRS.util.LRSAPIManager;
+
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.beans.factory.annotation.Value;
-import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
-import java.text.ParseException;
-import java.text.SimpleDateFormat;
-import java.util.*;
-import java.util.concurrent.TimeUnit;
-
 @Service
 public class AuthService {
     private static final Logger logger = LoggerFactory.getLogger(AuthService.class);
-<<<<<<< HEAD
-=======
-
->>>>>>> [feat] modify video date to register date
     @Autowired
     private JwtTokenUtil jwtTokenUtil;
     @Autowired
@@ -44,56 +50,46 @@ public class AuthService {
     private BCryptPasswordEncoder bCryptPasswordEncoder;
     @Autowired
     LRSAPIManager lrsapiManager;
+
     @Transactional
     public CMRespDto<?> singUp(SignUpRequestDto signUpRequestDto) {
-            logger.debug("signUpRequestDto : "+signUpRequestDto);
-            if (!emailDuplicateCheck(signUpRequestDto.getEmail()) && !nickNameDuplicateCheck(signUpRequestDto.getNickname())) {
-                UserMaster userMaster = UserMaster.builder()
-                        .nickname(signUpRequestDto.getNickname())
-                        .email(signUpRequestDto.getEmail())
-                        .provider(signUpRequestDto.getProvider())
-                        .role(Role.USER)
-                        .name(signUpRequestDto.getName())
-                        .userUuid(UUID.randomUUID().toString())
-                        .providerId(signUpRequestDto.getProviderId())
-                        .birthday(signUpRequestDto.getBirthday())
-                        .older_than_14(true)
-                        .service_agreement(true)
-                        .collect_info(true)
-                        .build();
-                userRepository.save(userMaster);
-                logger.debug("Sign up UserMaster is : "+userMaster);
+        logger.debug("signUpRequestDto : " + signUpRequestDto);
+        if (!emailDuplicateCheck(signUpRequestDto.getEmail())
+                && !nickNameDuplicateCheck(signUpRequestDto.getNickname())) {
+            UserMaster userMaster = UserMaster.builder().nickname(signUpRequestDto.getNickname())
+                    .email(signUpRequestDto.getEmail()).provider(signUpRequestDto.getProvider()).role(Role.USER)
+                    .name(signUpRequestDto.getName()).userUuid(UUID.randomUUID().toString())
+                    .providerId(signUpRequestDto.getProviderId()).birthday(signUpRequestDto.getBirthday())
+                    .older_than_14(true).service_agreement(true).collect_info(true).build();
+            userRepository.save(userMaster);
+            logger.debug("Sign up UserMaster is : " + userMaster);
 
-                PrincipalDetails principal = PrincipalDetails.create(userMaster);
-                String jwtToken = jwtTokenUtil.generateAccessToken(principal);
+            PrincipalDetails principal = PrincipalDetails.create(userMaster);
+            String jwtToken = jwtTokenUtil.generateAccessToken(principal);
 
-                // 현재 시간을 LRS timestamp 포멧에 맞게 변환
-                Date date = new Date(System.currentTimeMillis());
-                SimpleDateFormat sdf;
-                sdf = new SimpleDateFormat("yyyy-MM-dd'T'HH:mm:ss.SSSXXX");
-                sdf.setTimeZone(TimeZone.getTimeZone("KST"));
-                String text = sdf.format(date);
+            // 현재 시간을 LRS timestamp 포멧에 맞게 변환
+            Date date = new Date(System.currentTimeMillis());
+            SimpleDateFormat sdf;
+            sdf = new SimpleDateFormat("yyyy-MM-dd'T'HH:mm:ss.SSSXXX");
+            sdf.setTimeZone(TimeZone.getTimeZone("KST"));
+            String text = sdf.format(date);
 
-                // StatementDto빌드
-                StatementDTO statementDTO = StatementDTO.builder()
-                        .userId(userMaster.getUserUuid())
-                        .actionType("register")
-                        .sourceType("application")
-                        .timestamp(text)
-                        .build();
+            // StatementDto빌드
+            StatementDTO statementDTO = StatementDTO.builder().userId(userMaster.getUserUuid()).actionType("register")
+                    .sourceType("application").timestamp(text).build();
 
-                //LRS에 저장
-                List<StatementDTO> statementDTOList = new ArrayList<>();
-                statementDTOList.add(statementDTO);
-                try {
-                    lrsapiManager.saveStatementList(statementDTOList);
-                } catch (ParseException e) {
-                    return new CMRespDto<>(500,"LRS 전송 실패","실패");
-                }
-
-                return new CMRespDto<> (200 , "회원가입 완료",jwtToken);
+            // LRS에 저장
+            List<StatementDTO> statementDTOList = new ArrayList<>();
+            statementDTOList.add(statementDTO);
+            try {
+                lrsapiManager.saveStatementList(statementDTOList);
+            } catch (ParseException e) {
+                return new CMRespDto<>(500, "LRS 전송 실패", "실패");
             }
-        return new CMRespDto<> (201 , "회원가입 불가능","이메일이나 닉네임 중복 회원이 존재합니다.");
+
+            return new CMRespDto<>(200, "회원가입 완료", jwtToken);
+        }
+        return new CMRespDto<>(201, "회원가입 불가능", "이메일이나 닉네임 중복 회원이 존재합니다.");
     }
 
     @Transactional(readOnly = true)
@@ -119,14 +115,10 @@ public class AuthService {
         String text = sdf.format(date);
 
         // StatementDto빌드
-        StatementDTO statementDTO = StatementDTO.builder()
-                .userId(principalDetails.getUserUuid())
-                .actionType("withdrwawal")
-                .sourceType("application")
-                .timestamp(text)
-                .build();
+        StatementDTO statementDTO = StatementDTO.builder().userId(principalDetails.getUserUuid())
+                .actionType("withdrwawal").sourceType("application").timestamp(text).build();
 
-        //LRS에 저장
+        // LRS에 저장
         List<StatementDTO> statementDTOList = new ArrayList<>();
         statementDTOList.add(statementDTO);
         try {
@@ -136,14 +128,14 @@ public class AuthService {
         }
         return "True";
     }
+
     @Transactional
-    public CMRespDto<?> login(String providerId, AuthProvider provider,String ip) {
-        logger.debug("providerId is : "+providerId);
-        logger.debug("provider is : "+provider);
+    public CMRespDto<?> login(String providerId, AuthProvider provider, String ip) {
+        logger.debug("providerId is : " + providerId);
+        logger.debug("provider is : " + provider);
 
         System.out.println(ip);
-        Optional<UserMaster> userMasterOptional =
-                userRepository.findByProviderIdAndProvider(providerId, provider);
+        Optional<UserMaster> userMasterOptional = userRepository.findByProviderIdAndProvider(providerId, provider);
         logger.debug("userMasterOptional is : " + userMasterOptional);
         if (userMasterOptional.isPresent()) {
 
@@ -176,15 +168,11 @@ public class AuthService {
             String text = sdf.format(date);
 
             // StatementDto빌드
-            StatementDTO statementDTO = StatementDTO.builder()
-                    .userId(userMaster.getUserUuid())
-                    .actionType("enter")
-                    .sourceType("application")
-                    .timestamp(text)
-                    .build();
+            StatementDTO statementDTO = StatementDTO.builder().userId(userMaster.getUserUuid()).actionType("enter")
+                    .sourceType("application").timestamp(text).build();
             logger.debug("lrs statementDTO build success");
 
-            //LRS에 저장
+            // LRS에 저장
             List<StatementDTO> statementDTOList = new ArrayList<>();
             statementDTOList.add(statementDTO);
             try {
@@ -192,11 +180,11 @@ public class AuthService {
                 logger.debug("lrs save success");
 
             } catch (ParseException e) {
-                return new CMRespDto<>(500,"LRS 전송 실패","실패");
+                return new CMRespDto<>(500, "LRS 전송 실패", "실패");
             }
 
             List<String> IpList = userRepository.findAllByIp();
-            logger.debug("IpList is : "+IpList);
+            logger.debug("IpList is : " + IpList);
             if (IpList.contains(ip)) {
                 info.put("name", userMaster.getName());
                 return new CMRespDto<>(203, "관리자 로그인 성공", info);
@@ -208,8 +196,9 @@ public class AuthService {
         if (IpList.contains(ip)) {
             return new CMRespDto<>(202, "관리자 로그인 실패", ip);
         }
-        return new CMRespDto<>(201, "회원 가입이 안된 유저",null);
+        return new CMRespDto<>(201, "회원 가입이 안된 유저", null);
     }
+
     @Transactional
     public CMRespDto<?> logout(PrincipalDetails principalDetails) {
         Optional<UserMaster> userMasterOptional = userRepository.findByEmail(principalDetails.getEmail());
@@ -231,18 +220,19 @@ public class AuthService {
         try {
             lrsapiManager.saveStatementList(statementDTOList);
         } catch (ParseException e) {
-            return new CMRespDto<>(500,"LRS 전송 실패","실패");
+            return new CMRespDto<>(500, "LRS 전송 실패", "실패");
         }
-        return new CMRespDto<>(200,"로그아웃 성공",text);
+        return new CMRespDto<>(200, "로그아웃 성공", text);
     }
+
     @Transactional
-    public CMRespDto<?> modifyUserInfo(PrincipalDetails principalDetails, ModifyUserInfoDto modifyUserInfoDto){
+    public CMRespDto<?> modifyUserInfo(PrincipalDetails principalDetails, ModifyUserInfoDto modifyUserInfoDto) {
         Optional<UserMaster> userMasterOptional = userRepository.findByUserUuid(principalDetails.getUserUuid());
         UserMaster userMaster = userMasterOptional.get();
         userMaster.setNickname(modifyUserInfoDto.getNickname());
         userMaster.setBirthday(modifyUserInfoDto.getBirthday());
         userMaster.setEmail(modifyUserInfoDto.getEmail());
-        return new CMRespDto<>(200,"회원정보 수정 성공","성공");
+        return new CMRespDto<>(200, "회원정보 수정 성공", "성공");
     };
 
 }
