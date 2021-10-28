a6d0a0836a15ec8c8e4459629713461449320021
youngguen_song <younggeun_song@tmax.co.kr>
2021-10-28 15:27:43 +0900

    [fix] rm sl4fj

--- eTest/src/test/java/com/tmax/eTest/Contents/repository/RepositoryTest.java
+++ eTest/src/test/java/com/tmax/eTest/Contents/repository/RepositoryTest.java
@@ -1,10 +1,5 @@
 package com.tmax.eTest.Contents.repository;
 
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
-
-import com.tmax.eTest.Common.model.video.Video;
 import com.tmax.eTest.Common.repository.video.VideoRepository;
 
 import org.junit.Test;
@@ -14,12 +9,9 @@ import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabas
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
-import lombok.extern.slf4j.Slf4j;
-
 @RunWith(SpringRunner.class)
 @DataJpaTest
 @AutoConfigureTestDatabase(replace = AutoConfigureTestDatabase.Replace.NONE)
-@Slf4j
 public class RepositoryTest {
 
   @Autowired
@@ -28,12 +20,13 @@ public class RepositoryTest {
   @Test
   public void test() {
 
-    List<String> ids = new ArrayList<>(Arrays.asList("질문1-2", "보카7", "질문5-2", "단계2-4", "보카0", "질문1-1"));
-    log.debug(ids.size() + "");
-    Long st = System.currentTimeMillis();
-    log.debug("" + st);
-    List<Video> videos = videoRepository.findAllByRelatedIn(ids);
-    log.debug("Time: " + (System.currentTimeMillis() - st));
+    // List<String> ids = new ArrayList<>(Arrays.asList("질문1-2", "보카7", "질문5-2",
+    // "단계2-4", "보카0", "질문1-1"));
+    // log.debug(ids.size() + "");
+    // Long st = System.currentTimeMillis();
+    // log.debug("" + st);
+    // // List<Video> videos = videoRepository.findAllByRelatedIn(ids);
+    // log.debug("Time: " + (System.currentTimeMillis() - st));
 
   }
 }
