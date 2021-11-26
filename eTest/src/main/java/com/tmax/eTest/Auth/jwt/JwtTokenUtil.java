e7f84ffc729b71d72161f8ded60f18efe3614c5b
youngguen_song <younggeun_song@tmax.co.kr>
2021-11-26 21:26:06 +0900

    ebebebe

--- eTest/src/main/java/com/tmax/eTest/Auth/jwt/JwtTokenUtil.java
+++ eTest/src/main/java/com/tmax/eTest/Auth/jwt/JwtTokenUtil.java
@@ -1,82 +1,93 @@
 package com.tmax.eTest.Auth.jwt;
 
-
-import com.tmax.eTest.Auth.dto.PrincipalDetails;
-import io.jsonwebtoken.Claims;
-import io.jsonwebtoken.ExpiredJwtException;
-import io.jsonwebtoken.Jwts;
-import io.jsonwebtoken.SignatureAlgorithm;
-import org.springframework.beans.factory.annotation.Value;
-import org.springframework.security.core.GrantedAuthority;
-import org.springframework.security.core.userdetails.UserDetails;
-import org.springframework.stereotype.Component;
-
 import java.io.Serializable;
-import java.util.*;
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 import java.util.function.Function;
 
+import com.tmax.eTest.Auth.dto.PrincipalDetails;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.stereotype.Component;
+
+import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.SignatureAlgorithm;
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
 @Component
 public class JwtTokenUtil implements Serializable {
 
     private static final long serialVersionUID = -2550185165626007488L;
     @Value("${jwt.access.token.time}")
-    private long JWT_ACCESS_TOKEN_VALIDITY; //10분
+    private long JWT_ACCESS_TOKEN_VALIDITY; // 10분
     @Value("${jwt.refresh.token.time}")
-    private long JWT_REFRESH_TOKEN_VALIDITY; //일주일
+    private long JWT_REFRESH_TOKEN_VALIDITY; // 일주일
     @Value("${jwt.secret}")
     private String secret;
 
-    //retrieve username from jwt token
+    // retrieve username from jwt token
     public String getEmailFromToken(String token) {
         return getClaimFromToken(token, Claims::getSubject);
     }
 
-    //retrieve expiration date from jwt token
+    // retrieve expiration date from jwt token
     public Date getExpirationDateFromToken(String token) {
         return getClaimFromToken(token, Claims::getExpiration);
     }
+
     public <T> T getClaimFromToken(String token, Function<Claims, T> claimsResolver) {
         final Claims claims = getAllClaimsFromToken(token);
         return claimsResolver.apply(claims);
     }
 
-    //for retrieveing any information from token we will need the secret key
+    // for retrieveing any information from token we will need the secret key
     private Claims getAllClaimsFromToken(String token) {
         return Jwts.parser().setSigningKey(secret).parseClaimsJws(token).getBody();
     }
 
     public Map<String, Object> getUserParseInfo(String token) {
+        log.info(token);
         Claims parseInfo = Jwts.parser().setSigningKey(secret).parseClaimsJws(token).getBody();
+        log.info(parseInfo.toString());
         Map<String, Object> result = new HashMap<>();
         result.put("userUuid", parseInfo.get("userUuid"));
+        log.info((String) parseInfo.get("userUuid"));
         result.put("email", parseInfo.getSubject());
         result.put("role", parseInfo.get("role", List.class));
         return result;
     }
 
-    //check if the token has expired
+    // check if the token has expired
     public Boolean isTokenExpired(String token) {
         final Date expiration = getExpirationDateFromToken(token);
         return expiration.before(new Date());
     }
-    //generate token for user
+
+    // generate token for user
     public String generateAccessToken(PrincipalDetails principalDetails) {
         Map<String, Object> claims = new HashMap<>();
         List<String> li = new ArrayList<>();
-        for (GrantedAuthority a: principalDetails.getAuthorities()) {
+        for (GrantedAuthority a : principalDetails.getAuthorities()) {
             li.add(a.getAuthority());
         }
         claims.put("email", principalDetails.getEmail());
         claims.put("userUuid", principalDetails.getUserUuid());
-        claims.put("role",li);
-        return Jwts.builder().setClaims(claims).setSubject(principalDetails.getEmail()).setIssuedAt(new Date(System.currentTimeMillis()))
-                .setExpiration(new Date(System.currentTimeMillis() + JWT_ACCESS_TOKEN_VALIDITY*1000))
+        claims.put("role", li);
+        return Jwts.builder().setClaims(claims).setSubject(principalDetails.getEmail())
+                .setIssuedAt(new Date(System.currentTimeMillis()))
+                .setExpiration(new Date(System.currentTimeMillis() + JWT_ACCESS_TOKEN_VALIDITY * 1000))
                 .signWith(SignatureAlgorithm.HS512, secret).compact();
     }
 
     public String generateRefreshToken(String email) {
         return Jwts.builder().setIssuedAt(new Date(System.currentTimeMillis()))
-                .setExpiration(new Date(System.currentTimeMillis() + JWT_REFRESH_TOKEN_VALIDITY*1000))
+                .setExpiration(new Date(System.currentTimeMillis() + JWT_REFRESH_TOKEN_VALIDITY * 1000))
                 .signWith(SignatureAlgorithm.HS512, secret).compact();
     }
 
