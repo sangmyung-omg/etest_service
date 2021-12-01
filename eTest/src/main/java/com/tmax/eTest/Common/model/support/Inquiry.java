2f725bd57bb19fec89bb843901176ef042ccf34c
minjoon_lee <minjoon1995@naver.com>
2021-12-01 20:57:27 +0900

    refactoring : 안 쓰는 라이브러리 제거

--- eTest/src/main/java/com/tmax/eTest/Common/model/support/Inquiry.java
+++ eTest/src/main/java/com/tmax/eTest/Common/model/support/Inquiry.java
@@ -1,31 +1,13 @@
 package com.tmax.eTest.Common.model.support;
 
-import java.time.LocalDateTime;
-import java.util.List;
-
-import javax.persistence.Column;
-import javax.persistence.Entity;
-import javax.persistence.FetchType;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
-import javax.persistence.JoinColumn;
-import javax.persistence.ManyToOne;
-import javax.persistence.OneToMany;
-import javax.persistence.PrePersist;
-import javax.persistence.SequenceGenerator;
-import javax.persistence.Table;
-
 import com.fasterxml.jackson.annotation.JsonBackReference;
-import com.fasterxml.jackson.annotation.JsonIgnore;
-import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import com.fasterxml.jackson.annotation.JsonManagedReference;
 import com.tmax.eTest.Common.model.user.UserMaster;
 import lombok.*;
 
+import javax.persistence.*;
 import java.time.LocalDateTime;
 import java.util.List;
-import javax.persistence.*;
 
 @Entity
 @Getter
