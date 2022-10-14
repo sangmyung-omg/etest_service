1b3d76a8a8343e6a37e476507e7e5ec66d5f525b
sangmyung-omg <64184699+sangmyung-omg@users.noreply.github.com>
2022-10-14 17:04:05 +0900

    Delete eTest/src/main/java/com/tmax/eTest/Push directory

--- eTest/src/main/java/com/tmax/eTest/Push/repository/UserNotificationConfigRepository.java
+++
@@ -1,8 +0,0 @@
-package com.tmax.eTest.Push.repository;
-
-import com.tmax.eTest.Push.model.UserNotificationConfig;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface UserNotificationConfigRepository extends JpaRepository<UserNotificationConfig, String> {}
