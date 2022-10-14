07cb8b44bcf194699c0fac113031126fde8ef33c
sangmyung-omg <64184699+sangmyung-omg@users.noreply.github.com>
2022-10-14 17:04:28 +0900

    Delete eTest/src/main/java/com/tmax/eTest/TestStudio directory

--- eTest/src/main/java/com/tmax/eTest/TestStudio/repository/DiagCurriculumRepositoryTs.java
+++
@@ -1,9 +0,0 @@
-package com.tmax.eTest.TestStudio.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.tmax.eTest.Common.model.problem.DiagnosisCurriculum;
-
-public interface DiagCurriculumRepositoryTs extends JpaRepository<DiagnosisCurriculum, Integer> {
-
-}	
