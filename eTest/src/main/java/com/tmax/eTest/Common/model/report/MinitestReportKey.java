daa774affbfc84968f87d65732106012039ed6cc
sangmyung-omg <64184699+sangmyung-omg@users.noreply.github.com>
2022-10-14 17:30:43 +0900

    Delete eTest/src/main/java/com/tmax/eTest/Common/model/report directory

--- eTest/src/main/java/com/tmax/eTest/Common/model/report/MinitestReportKey.java
+++
@@ -1,8 +0,0 @@
-package com.tmax.eTest.Common.model.report;
-
-import java.io.Serializable;
-import java.sql.Timestamp;
-
-public class MinitestReportKey implements Serializable {
-	String minitestId;
-}
