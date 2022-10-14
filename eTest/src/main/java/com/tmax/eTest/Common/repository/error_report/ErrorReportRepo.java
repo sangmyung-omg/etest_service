2f8af8d5ec136678e5345c454f12fe894913d3df
sangmyung-omg <64184699+sangmyung-omg@users.noreply.github.com>
2022-10-14 17:29:01 +0900

    Delete eTest/src/main/java/com/tmax/eTest/Common/repository/error_report directory

--- eTest/src/main/java/com/tmax/eTest/Common/repository/error_report/ErrorReportRepo.java
+++
@@ -1,8 +0,0 @@
-package com.tmax.eTest.Common.repository.error_report;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.tmax.eTest.Common.model.error_report.ErrorReport;
-
-public interface ErrorReportRepo extends JpaRepository<ErrorReport, Long>{
-    
-}
